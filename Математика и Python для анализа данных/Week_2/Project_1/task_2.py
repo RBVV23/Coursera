87174631609b02f3ab3aeab5b9bdd353db829813
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-12 21:20:03 +0300

    1208 2119

--- Математика и Python для анализа данных/Week_2/Project_1/task_2.py
+++ Математика и Python для анализа данных/Week_2/Project_1/task_2.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import linalg
 
 def f(x):
+
     return np.sin(x/5)*np.exp(x/10) + 5*np.exp(-x/2)
 
 x = np.arange(0, 15.05, 0.05)
@@ -21,6 +22,51 @@ print(y_sol)
 
 A = np.array([ [1, x_sol[0]], [1, x_sol[1]] ])
 print(A)
+b = np.array([y_sol[0], y_sol[1]])
+print(b)
 
-# linalg.solve(x_sol, y_sol)
+W=np.array(linalg.solve(A, b))
+print('W:')
+print(W[0], W[1])
+
+def f1_pol(inp, W_matr):
+    # print('x=' + str(inp))
+    w_0 = W_matr[0]
+    # print('w_0=' + str(w_0))
+    w_1 = W_matr[1]
+    # print('w_1=' + str(w_1))
+    return w_1*inp + w_0
+
+x = np.arange(0, 15.05, 0.05)
+y_1 = f1_pol(x, W)
+# print(x)
+# f_pol(1, W)
+# print(f_pol(1, W))
+# f_pol(15, W)
+# print(f_pol(15, W))
+
+plt.plot(x, y, x, y_1, x_sol, y_sol, 'o')
+plt.show()
+
+x_sol = np.array([1.,8., 15.])
+y_sol = f(x_sol)
+
+A = np.array([ [1, x_sol[0], x_sol[0]**2],
+               [1, x_sol[1], x_sol[1]**2],
+               [1, x_sol[2], x_sol[2]**2] ])
+
+b = np.array([y_sol[0], y_sol[1], y_sol[2]])
+W=np.array(linalg.solve(A, b))
+print('W:')
+print(W[0], W[1], W[2])
+
+def f2_pol(inp, W_matr):
+    w_0 = W_matr[0]
+    w_1 = W_matr[1]
+    w_2 = W_matr[2]
+    return w_2*inp*inp + w_1*inp + w_0
+
+y_2 = f2_pol(x, W)
+plt.plot(x, y, x, y_2, x_sol, y_sol, 'o')
+plt.show()
 
