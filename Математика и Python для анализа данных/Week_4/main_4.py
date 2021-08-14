45c7c8e399c8cb57c2652cf5914e66c8142479e9
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-14 16:54:03 +0300

    1408 - 1653

--- Математика и Python для анализа данных/Week_4/main_4.py
+++ Математика и Python для анализа данных/Week_4/main_4.py
@@ -2,7 +2,43 @@ import numpy as np
 print(np.__version__)
 import matplotlib.pyplot as plt
 # print(plt.__version__)
+import scipy.stats as sts
 import pandas as pd
 print(pd.__version__)
-import scipy as sp
-print(sp.__version__)
+from collections import Counter
+
+T = np.random.choice([1, 2, 3, 4, 5, 6], 100)
+print(T)
+c = Counter(T)
+print("Число выпадений каждой из сторон:")
+print(c)
+print("Вероятности выпадений каждой из сторон:")
+print(c.items())
+print()
+for k, v in c.items():
+    print('k=' + str(k) + ' v=' + str(v/100.0))
+print({k: v/100 for k, v in c.items()})
+
+T = sts.norm(1,10).rvs(100)
+# T_2 = T.rvs(100)
+print(T)
+
+x = np.linspace(-4,4,100)
+cdf = sts.norm(0,1).cdf(x)
+plt.plot(x, cdf, label='theoretical CDF')
+plt.ylabel('$f(x)$')
+plt.xlabel('$x$')
+plt.legend(loc='upper left')
+plt.show()
+
+plt.hist(T, density=True)
+plt.ylabel('fraction of samples')
+plt.xlabel('$x$')
+plt.show()
+
+plt.hist(T, bins=3, density=True)
+plt.ylabel('fraction of samples')
+plt.xlabel('$x$')
+plt.show()
+
+
