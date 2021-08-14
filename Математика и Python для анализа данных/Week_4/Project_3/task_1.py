c55040463d18cf38b281da6dc960f1c32099d447
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-14 23:32:42 +0300

    1408 - 2332

--- Математика и Python для анализа данных/Week_4/Project_3/task_1.py
+++ Математика и Python для анализа данных/Week_4/Project_3/task_1.py
@@ -3,7 +3,9 @@ np.__version__
 import matplotlib.pyplot as plt
 
 
-x=np.array([1, 2, 3, 4])
+x=np.array([1, 2, 3, 4, 5, 6, 7, 8])
 y=x**2
 plt.plot(x, y)
+plt.title('123')
+plt.ylabel('yyyy')
 plt.show()
