d6d613f516bff62193a822ed212b483a238a0ed5
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-15 23:27:37 +0300

    1508 - 2327

--- Математика и Python для анализа данных/Week_4/Project_3/task_1.py
+++ Математика и Python для анализа данных/Week_4/Project_3/task_1.py
@@ -9,7 +9,7 @@ disp = sts.trapezoid.var(c=0.15, d=0.35, loc=0, scale=1)
 print('Теоретическая дисперсия составляет: ', disp)
 SKO = disp**(0.5)
 print('Теоретическое СКО составляет: ', SKO)
-
+plt.show()
 x = np.linspace(0,1)
 n = 5
 sample_mean = []
@@ -18,11 +18,14 @@ for i in range(1000):
 #     print(T_n)
     sample_mean.append(np.mean(T_n))
 # print(sample_mean)
-sample_mean = sample_mean
-plt.hist(sample_mean, density=True, stacked = True, label='n=5') #, label='Выборка')
+plt.hist(sample_mean, label='n=5')
 pdf = sts.norm.pdf(x, loc=mn, scale=SKO)
 plt.plot(x, pdf, label='Нормальное распределение',  linewidth=3 )
 plt.ylabel('Распределение выборочных средних')
 plt.xlabel('$x_{cp}$')
 plt.legend(loc='upper right')
 plt.show()
+
+# A = [10, 3, 6, 30, 49, 4, 4, 6, 90, 7]
+# N = plt.hist(A, label='n=5') #, label='Выборка')
+# plt.show()
