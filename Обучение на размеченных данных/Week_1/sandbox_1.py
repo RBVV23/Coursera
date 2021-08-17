9ffc17e751083db897e9637c5d4a4dcfdd34daf5
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-17 23:09:43 +0300

    1708

--- Обучение на размеченных данных/Week_1/sandbox_1.py
+++ Обучение на размеченных данных/Week_1/sandbox_1.py
@@ -1,2 +1,18 @@
 import pandas as pd
 pd.__version__
+import seaborn as sns
+sns.__version__
+import numpy as np
+np.__version__
+import matplotlib.pyplot as plt
+# plt.__version__
+
+# data = pd.read_csv('weights_heights.csv')
+data = pd.read_csv('weights_heights.csv',  index_col='Index')
+
+print(data.head())
+# print(data.shape)
+# print(data.info())
+
+data.plot(y='Height', kind='hist', color='red', title='Распределние роста подростков (дюймы)')
+plt.show()
