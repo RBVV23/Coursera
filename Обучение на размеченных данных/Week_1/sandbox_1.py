814166388b89e91ae01714728aa4fd603a956824
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-18 16:23:53 +0300

    1808 1623

--- Обучение на размеченных данных/Week_1/sandbox_1.py
+++ Обучение на размеченных данных/Week_1/sandbox_1.py
@@ -15,6 +15,15 @@ def make_bmi(height, weight):
     bmi = weight_pound/(height_inch**2)
     return bmi
 
+def weight_category(weight):
+    pass
+    cat = 2
+    if weight < 120:
+        cat = 1
+    elif weight >= 150:
+        cat = 3
+    return cat
+
 
 # data = pd.read_csv('weights_heights.csv')
 data = pd.read_csv('weights_heights.csv',  index_col='Index')
@@ -23,11 +32,57 @@ print(data.head())
 # print(data.shape)
 # print(data.info())
 
-data.plot(y='Height', kind='hist', color='red', title='Распределние роста подростков (дюймы)')
+## data.plot(y='Height', kind='hist', color='red', title='Распределние роста подростков (дюймы)')
 
-data['BMI'] = make_bmi(data['Height'], data['Weight'])
-# print(data.head())
+data['BMI'] = make_bmi(data['Height'], data['Weight']) # Саня, здесь я применил свою функцию к столбцам "в лоб" и питон сам все сделал
+# data['weight_cat'] = weight_category(data['Weight']) # Почему тогда здесь не прокатил аналогичный способ?
+
+data['weight_cat'] = data['Weight'].apply(weight_category) # здесь я использовал apply, чтобы применить функцию к целому столбцу
+# и можно было функцию make_bmi тож сделать через apply?
+
+print(data.head())
 
 # sns_plot = sns.pairplot(data)
-sns_plot = sns.pairplot(data[['Height', 'Weight', 'BMI']])
-sns_plot.savefig('pairplot.png')
+## sns_plot = sns.pairplot(data[['Height', 'Weight', 'BMI']])
+# sns_plot.savefig('pairplot.png')
+## plt.show()
+
+
+## boxplot = sns.boxplot(y='Height', x='weight_cat', data=data, orient='v')
+## boxplot.set_xlabel('Весовая категория')
+## boxplot.set_ylabel('Рост')
+## plt.show()
+
+## data.plot(x='Weight', y='Height', kind='scatter')
+## plt.show()
+
+Y = data['Height']
+print(len(Y))
+# print(Y[1])
+X = np.linspace(0, 200, 25000)
+print('START')
+
+def my_error(w0, w1):
+    sum = 0
+    for i in range(1, len(Y)):
+        # print('\tX = ' + str(X[i]))
+        # print('\t' + 'myX = ' + str(w0 + w1*X[i]))
+        # print('\tY = ' + str(Y[i]))
+        er = (Y[i] - (w0 + w1*X[i]))**2
+        # print('\t\tError = ' + str(er))
+        sum += er
+    return sum
+print('pre', my_error(60, 0.05))
+
+X = np.linspace(0, 200, 25000)
+Y = data['Height']
+w_0 = 5
+w_1 = []
+dw = 0.01
+Er = []
+for i in range(100):
+    w_1.append(dw*i)
+    # print(w_1[i])
+    Er.append(my_error(w_0, w_1[i]))
+plt.plot(w_1, Er)
+plt.show()
