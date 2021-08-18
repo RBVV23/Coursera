7d60491313a7a24e05664e338ce6b0d51f1691ee
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-18 11:54:46 +0300

    1808

--- Обучение на размеченных данных/Week_1/sandbox_1.py
+++ Обучение на размеченных данных/Week_1/sandbox_1.py
@@ -7,6 +7,15 @@ np.__version__
 import matplotlib.pyplot as plt
 # plt.__version__
 
+def make_bmi(height, weight):
+    METER_TO_INCH = 39.37
+    KILO_TO_POUND = 2.20462
+    height_inch = height / METER_TO_INCH
+    weight_pound = weight / KILO_TO_POUND
+    bmi = weight_pound/(height_inch**2)
+    return bmi
+
+
 # data = pd.read_csv('weights_heights.csv')
 data = pd.read_csv('weights_heights.csv',  index_col='Index')
 
@@ -15,4 +24,10 @@ print(data.head())
 # print(data.info())
 
 data.plot(y='Height', kind='hist', color='red', title='Распределние роста подростков (дюймы)')
-plt.show()
+
+data['BMI'] = make_bmi(data['Height'], data['Weight'])
+# print(data.head())
+
+# sns_plot = sns.pairplot(data)
+sns_plot = sns.pairplot(data[['Height', 'Weight', 'BMI']])
+sns_plot.savefig('pairplot.png')
