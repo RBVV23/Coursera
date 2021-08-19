10856bb71e15acdfc4dcff4a6c6e225f331102dc
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-19 20:37:24 +0300

    1908 2037

--- Обучение на размеченных данных/Week_1/Project_1/task_1.py
+++ Обучение на размеченных данных/Week_1/Project_1/task_1.py
@@ -4,6 +4,28 @@ import matplotlib.pyplot as plt
 from scipy import optimize
 
 
-advert_data = pd.read_csv('advertising.csv')
-print(advert_data.head())
-print(advert_data.info())
+adver_data = pd.read_csv('advertising.csv')
+print(adver_data.head())
+print(adver_data.info())
+
+# adver_data = adver_data.head()
+X = np.array([adver_data['TV'], adver_data['Radio'], adver_data['Newspaper']]) # Ваш код здесь
+X = X.T
+y = np.array(adver_data['Sales']) # Ваш код здесь
+# print(X)
+
+means = np.mean(X, axis=0)
+# print(means)
+X = X - means
+stds = np.std(X, axis=0) # Ваш код здесь
+# print(stds)
+X = X /stds
+
+y = np.array([1, 1, 1, 1, 1])
+y_pred = np.array([1.2, 0.8, 1, 1, 1.8])
+
+
+def mserror(y, y_pred):
+    return np.mean((y - y_pred)**2)
+
+print(mserror(y, y_pred))
