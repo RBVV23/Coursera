291ce8642e00df04d1b296ff5e4234cfa326f670
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-28 18:29:34 +0300

    2808 1827

--- Обучение на размеченных данных/Week_2/Lesson_3.py
+++ Обучение на размеченных данных/Week_2/Lesson_3.py
@@ -4,3 +4,4 @@ from sklearn import model_selection, datasets, linear_model, metrics
 import matplotlib.pyplot as plt
 import numpy as np
 
+blobs = datasets.make_blobs(centers=2, cluster_std=5.5, random_state=1)
