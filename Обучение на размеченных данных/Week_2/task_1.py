81306356a1119f7489855a6a7316c9cb8095277b
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-08-30 17:32:33 +0300

    3008 1732

--- Обучение на размеченных данных/Week_2/task_1.py
+++ Обучение на размеченных данных/Week_2/task_1.py
@@ -1,10 +1,10 @@
-from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.linear_model import LinearRegression, Lasso, Ridge, LassoCV
 from sklearn.preprocessing import scale
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import re
+
 
 data = pd.read_csv('bikes_rent.csv')
 pd.set_option('display.width', 1000)
@@ -22,15 +22,6 @@ print('\n======================== МАТРИЦА КОРРЕЛЯЦИЙ ПИРСО
 print(data.corr())
 print('\t ========================================================================================================\n')
 
-str_task = 'temp, atemp, hum, windspeed(mph), windspeed(ms) и cnt'
-# my_str = re.split('[^a-z]', str_task)
-# while True:
-#     try:
-#         my_str.remove('')
-#     except ValueError:
-#         break
-# print(my_str)
-
 
 print('\n======================== СРЕДНИЕ ЗНАЧЕНИЯ ПО ПРИЗНАКАМ ========================')
 print(np.mean(data, axis=0))
@@ -44,3 +35,62 @@ regressor = LinearRegression()
 regressor.fit(X, y)
 for col, cof in zip(data.columns, regressor.coef_):
     print('{}: {:.3f}'.format(col, cof))
+
+l1_regressor = Lasso(random_state=123)
+l1_regressor.fit(X, y)
+for col, cof in zip(data.columns, l1_regressor.coef_):
+    print('{}: {:.3f}'.format(col, cof))
+
+l2_regressor = Ridge(random_state=123)
+l2_regressor.fit(X, y)
+for col, cof in zip(data.columns, l2_regressor.coef_):
+    print('{}: {:.3f}'.format(col, cof))
+
+alphas = np.arange(1, 500, 50)
+coefs_lasso = np.zeros((alphas.shape[0], X.shape[1]))
+coefs_ridge = np.zeros((alphas.shape[0], X.shape[1]))
+for a in enumerate(alphas):
+    reg_r = Ridge(random_state=123, alpha=a[1])
+    reg_r.fit(X, y)
+    coefs_ridge[a[0],:]=reg_r.coef_
+    reg_l = Lasso(random_state=123, alpha=a[1])
+    reg_l.fit(X, y)
+    coefs_lasso[a[0],:]=reg_l.coef_
+
+plt.figure(figsize=(8, 5))
+for coef, feature in zip(coefs_lasso.T, data.columns):
+    plt.plot(alphas, coef, label=feature, color=np.random.rand(3))
+plt.legend(loc="upper right", bbox_to_anchor=(1.4, 0.95))
+plt.xlabel("alpha")
+plt.ylabel("feature weight")
+plt.title("Lasso")
+plt.show()
+
+plt.figure(figsize=(8, 5))
+for coef, feature in zip(coefs_ridge.T, data.columns):
+    plt.plot(alphas, coef, label=feature, color=np.random.rand(3))
+plt.legend(loc="upper right", bbox_to_anchor=(1.4, 0.95))
+plt.xlabel("alpha")
+plt.ylabel("feature weight")
+plt.title("Ridge")
+plt.show()
+
+alphas = np.arange(1, 100, 5)
+new_regressor = LassoCV(cv=3, alphas=alphas)
+new_regressor.fit(X, y)
+print('alpha: ', new_regressor.alpha_)
+for col, cof in zip(data.columns, new_regressor.coef_):
+    print('{}: {:.3f}'.format(col, cof))
+
+plt.plot(new_regressor.alphas_, np.mean(new_regressor.mse_path_, axis=1))
+plt.show()
+
+for n in range(3):
+    plt.figure(figsize=(8, 5))
+    minMSE = np.min(new_regressor.mse_path_[:,n])
+    alpha_n = np.argmin(new_regressor.mse_path_[:,n])
+    alpha_1 = new_regressor.alphas_[alpha_n]
+    my_label = 'min(MSE) = {}, alpha = {}'.format(minMSE, alpha_1)
+    plt.plot(new_regressor.alphas_, new_regressor.mse_path_[:,n], label=my_label)
+    plt.legend(loc="upper right")
+    plt.show()
