0d83499f2288f5e9ca266ba3f366a27f02f9d840
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-02 13:27:52 +0300

    0209 1337

--- Обучение на размеченных данных/Week_3/Project_1/main.py
+++ Обучение на размеченных данных/Week_3/Project_1/main.py
@@ -7,3 +7,23 @@ import matplotlib.pyplot as plt
 
 import warnings
 warnings.filterwarnings('ignore')
+
+def calculate_means(numeric_data):
+    means = np.zeros(numeric_data.shape[1])
+    for j in range(numeric_data.shape[1]):
+        to_sum = numeric_data.iloc[:,j]
+        indices = np.nonzero(~numeric_data.iloc[:,j].isnull().values)[0] # - авторская версия
+        correction = np.amax(to_sum[indices])
+        for i in indices:
+            means[j] += to_sum[i]
+        means[j] /= indices.size
+    return pd.Series(means, numeric_data.columns)
+
+
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_rows', 250)
+pd.set_option('display.max_columns', 100)
+
+
+data = pd.read_csv('data.csv')
+print(data.shape)
