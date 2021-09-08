20d0437ffe1d68056cfe9fa16f74a90496a49324
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-08 23:32:52 +0300

    0809 2332

--- Обучение на размеченных данных/Week_4/Project_2/main.py
+++ Обучение на размеченных данных/Week_4/Project_2/main.py
@@ -1,6 +1,8 @@
 from sklearn import ensemble, model_selection
 import numpy as np
+print(np.__version__)
 import pandas as pd
+print(pd.__version__)
 import xgboost as xgb
 import matplotlib.pyplot as plt
 
