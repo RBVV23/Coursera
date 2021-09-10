533afb0ce2832ae11f0c35a71700681f16b89ac2
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-10 19:51:49 +0300

    1009 1951

--- Обучение на размеченных данных/Week_5/Project_1/main.py
+++ Обучение на размеченных данных/Week_5/Project_1/main.py
@@ -1,7 +1,17 @@
-import pybrain
+import pybrain as pybrain
+
+print(pybrain.__version__)
+
 import random
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import normalize
+from sklearn.model_selection import train_test_split
 import numpy as np
 
 import matplotlib.pyplot as plt
+
+from pybrain.datasets import ClassificationDataSet # Структура данных pybrain
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.supervised.trainers import BackpropTrainer
+from pybrain.structure.modules import SoftmaxLayer
+from pybrain.utilities import percentError
