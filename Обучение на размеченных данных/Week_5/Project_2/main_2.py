78a7f117ad7af4c3b5ea4fb7ad3cc60eb77d7129
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-13 13:04:21 +0300

    1309 1304

--- Обучение на размеченных данных/Week_5/Project_2/main_2.py
+++ Обучение на размеченных данных/Week_5/Project_2/main_2.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import model_selection, metrics, datasets, linear_model, tree
+from sklearn import model_selection, metrics, datasets, linear_model, tree, naive_bayes
 from math import ceil
 import numpy as np
 
@@ -9,7 +9,7 @@ def my_print_features(dataset, in_line=3):
     # print('n_lines = ', n_lines)
     n_last_line = len(feature_names) - in_line*(n_lines-1)
     # print('n_last_line = ', n_last_line)
-    print('Список признаков: ')
+    print('\nСписок признаков: ')
     for n in range(n_lines-1):
         line = ''
         for i in range(in_line):
@@ -22,22 +22,34 @@ def my_print_features(dataset, in_line=3):
         ind = n * in_line + i
         line += '\t' + str(feature_names[ind])
     print(line)
+    print()
+def my_print_targets(dataset, r=2):
+    target_names = dataset.target_names
+    print('\nЦелевая переменная принимает значения (метки): ')
+    for target in enumerate(target_names):
+        print('\t {} ({}): {} раз(-a) - {}%'.format(target[1], target[0], sum(dataset.target == target[0]),
+              round(sum(dataset.target == target[0])/len(dataset.target)*100, r)))
+    print()
+
+def write_answer(answer, number):
+    name = 'ans{}.txt'.format(number)
+    with open(name, "w") as fout:
+        fout.write(str(answer))
 
 digits = datasets.load_digits()
-# my_print_features(digits, 8)
-
-def my_print_targets(dataset, classification=True):
-    target_names = dataset.target_names
-    print('Целевая переменная принимает значения (метки): ')
-    for target in enumerate(target_names):
-        print('\t {} ({}) - {} раз(-a)'.format(target[1], target[0], sum(dataset.target == target[0])))
-
-
-
-
-
 breast_cancer = datasets.load_breast_cancer()
-my_print_targets(digits)
-# print(sum(breast_cancer.target == 0))
+# my_print_features(digits, 8)
+# my_print_targets(digits, 1)
 
-#357 ==1 - benign
+X = digits.data
+y = digits.target
+
+classifer = naive_bayes.BernoulliNB()
+res = model_selection.cross_val_score(classifer, X, y)
+answer_1 = res.mean()
+print('answer_1 = ', res)
+write_answer(answer_1, 1)
+
+
+
+
