3caba5eefdb4c34f6e2abde87c0416bbbe67d9f5
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-14 15:35:33 +0300

    1409 1535

--- Обучение на размеченных данных/Week_5/Project_3/main_3.py
+++ Обучение на размеченных данных/Week_5/Project_3/main_3.py
@@ -1,12 +1,11 @@
 import numpy as np
-from sklearn import model_selection, metrics, datasets, linear_model, tree
-
-from pybrain.datasets import ClassificationDataSet
-from pybrain.tools.shortcuts import buildNetwork
-from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure.modules import SoftmaxLayer
-from pybrain.utilities import percentError
+from sklearn import model_selection, metrics, datasets, linear_model, tree, ensemble
 from sklearn.model_selection import train_test_split
+
+def write_answer(answer, number):
+    name = 'ans{}.txt'.format(number)
+    with open(name, "w") as fout:
+        fout.write(str(answer))
 
 def my_euclid_dist(obj_1, obj_2):
     if len(obj_1) == len(obj_2):
@@ -20,8 +19,6 @@ def my_euclid_dist(obj_1, obj_2):
     dist = dist**0.5
     return dist
 
-
-
 def my_1NN(obj, X, y):
     N = X.shape[0]
     matrix = np.ones((N,2))*-1
@@ -30,6 +27,11 @@ def my_1NN(obj, X, y):
         matrix[i][1] = y[i]
     ind = matrix[:,0].argmin()
     return int(matrix[ind,1])
+
+def my_error_counter(y_true, y_predict):
+    y_true = np.array(y_true)
+    y_predict = np.array(y_predict)
+    return np.sum([y_true != y_predict]) / len(y_true)
 
 
 
@@ -45,8 +47,18 @@ y_predict = []
 for obj in X_test:
     prediction = my_1NN(obj, X_train, y_train)
     y_predict.append(prediction)
-print('y_test: ', y_test)
-print('y_predict: ', y_predict)
 
 
+# print('y_test: ', y_test)
+# print('y_predict: ', y_predict)
+answer_1 = my_error_counter(y_test, y_predict)
+print('answer_1 = ', answer_1)
+write_answer(answer_1, 1)
 
+
+classifer = ensemble.RandomForestClassifier(n_estimators=1000, random_state=0)
+classifer.fit(X_train, y_train)
+y_predict = classifer.predict(X_test)
+answer_2 = my_error_counter(y_test, y_predict)
+print('answer_2 = ', answer_2)
+write_answer(answer_2, 2)
---
+++ Обучение на размеченных данных/Week_5/sandbox.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+A = np.array([1, 2, 3, 4, 5])
+B = np.array([1, 0, 3, 7, 5])
+
+print(np.sum([A!=B])/len(A))
