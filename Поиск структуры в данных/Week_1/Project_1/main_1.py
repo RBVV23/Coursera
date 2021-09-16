77a257c08865c03265f3f09510412569de354cd7
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-16 17:05:11 +0300

    1609 1705

--- Поиск структуры в данных/Week_1/Project_1/main_1.py
+++ Поиск структуры в данных/Week_1/Project_1/main_1.py
@@ -1,18 +1,43 @@
 import pandas as pd
 import numpy as np
+from sklearn.cluster import MeanShift
 
-# def my_pandas_info()
+
+def dist_to_nearest_office(offices, cluster_center, debug=False):
+    dists = []
+    if debug:
+        print('Center: X = {}; Y = {}'.format(cluster_center[0], cluster_center[1]))
+    for item in offices:
+        dist = ((item[0] - cluster_center[0])**2 + (item[1] - cluster_center[1])**2 )**0.5
+        dists.append(dist)
+        if debug:
+            print('\tOffice: X = {}; Y = {}'.format(item[0], item[1]))
+            print('\tDistantion = ', dist)
+    min_dist = min(dists)
+    ind = dists.index(min_dist)
+    if debug:
+        print('\t\tNearest office #{}: {}'.format(ind, offices[ind]))
+        print('\t\tMinimal distance: {}'.format(min_dist))
+    return min_dist, ind
 
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_rows', 250)
 pd.set_option('display.max_columns', 100)
 
+debug=False
 
 data = pd.read_csv('checkins.csv', sep='|', header=0, skipinitialspace=True)
 columns = data.columns
-print('data.shape = ', data.shape)
+# print('data.shape = ', data.shape)
 data = data.drop(data.index[[0, -1]])
 
+coordinates_of_offices = [[33.751277, -118.188740],
+                          [25.867736, -80.324116],
+                          [51.503016, -0.075479],
+                          [52.378894, 4.885084],
+                          [39.366487, 117.036146],
+                          [-33.868457, 151.205134]]
+cities_offices = ['Los Angeles', 'Miami', 'London', 'Amsterdam', 'Beijing', 'Sydney']
 
 my_columns = []
 for col in columns:
@@ -23,8 +48,66 @@ for col in columns:
 data = data.astype({'id': int, 'user_id': int, 'venue_id': int})
 
 print(data.head())
-# print(data.info())
+data = data.dropna(subset=['latitude', 'longitude'])
 
-for iter in data['latitude']:
-        if iter.isna():
-            print(iter)
+# print(data.head())
+print('data.shape = ', data.shape)
+
+small_data = data.iloc[0:100000]
+data = data.iloc[90000:95000]
+# data = data.iloc[0:100000]
+
+# print(small_data.head())
+# print('small_data.shape = ', small_data.shape)
+
+X = data.iloc[:, [3,4]]
+# print(X.head())
+clustering = MeanShift(bandwidth=0.1).fit(X)
+print('clustering.labels: ', clustering.labels_)
+print('clustering.cluster_centers: ', clustering.cluster_centers_[:10])
+cluster_centers = clustering.cluster_centers_
+unique, counts = np.unique(clustering.labels_, return_counts=True)
+print('Количество кластеров: {} '.format(len(unique)))
+my_dict = dict(zip(unique, counts))
+
+print(my_dict)
+
+keys = my_dict.keys()
+
+i = 0
+clusters = {}
+for key in keys:
+    if my_dict[key] >= 15:
+        i += 1
+        print('#{}. Кластер "{}" : {}'.format(i, key, my_dict[key]))
+        clusters[key] = my_dict[key]
+
+min_dists = []
+office_inds = []
+for n in clusters:
+    point = cluster_centers[n]
+    min_dist, office_ind = dist_to_nearest_office(coordinates_of_offices, point, debug=False)
+    if debug:
+        print('Кластер # {}\nБлижайший офис: {} ({})'.format(n, cities_offices[office_ind], min_dist))
+    min_dists.append(min_dist)
+    office_inds.append(office_ind)
+
+# min_dists_sort = np.sort(min_dists)[:20]
+min_dists_sort = np.sort(min_dists)[:5]
+print(min_dists_sort)
+# print(min_dists[:5])
+
+print('\n\t\t\tРЕЗУЛЬТАТЫ:\n')
+for i, dist in enumerate(min_dists_sort):
+    print('{}-е место:'.format(i+1))
+    n = min_dists.index(dist)
+    print('\tКластер №{} (X = {}, Y = {})'.format(n, cluster_centers[n][0], cluster_centers[n][1]))
+    city = cities_offices[office_inds[n]]
+    X = coordinates_of_offices[office_inds[n]][0]
+    Y = coordinates_of_offices[office_inds[n]][1]
+    print('\t\tБлижайший офис: {} (X = {}, Y = {})'.format(city, X, Y))
+    print('\t\tРасстояние: {}'.format(dist))
+
+
+
+
--- Поиск структуры в данных/Week_1/sandbox_1.py
+++ Поиск структуры в данных/Week_1/sandbox_1.py
@@ -4,7 +4,47 @@ import itertools
 permutations = itertools.permutations([0, 1, 2])
 # print(list(permutations))
 
-for a,b,c in permutations:
-    print('a = {}, b = {}, c = {}'.format(a,b,c))
-    mapping = mapping = {2:a, 1:b, 0:c}
-    print(mapping)
+# for a,b,c in permutations:
+#     print('a = {}, b = {}, c = {}'.format(a,b,c))
+#     mapping = mapping = {2:a, 1:b, 0:c}
+#     print(mapping)
+
+
+
+# def dist_to_nearest_office(offices, cluster_center, debug=False):
+#     dists = []
+#     if debug:
+#         print('Center: X = {}; Y = {}'.format(cluster_center[0], cluster_center[1]))
+#     for item in offices:
+#         dist = ((item[0] - cluster_center[0])**2 + (item[1] - cluster_center[1])**2 )**0.5
+#         dists.append(dist)
+#         if debug:
+#             print('\tOffice: X = {}; Y = {}'.format(item[0], item[1]))
+#             print('\tDistantion = ', dist)
+#     min_dist = min(dists)
+#     ind = np.argmin(dist)
+#     if debug:
+#         print('\t\tNearest office #{}: {}'.format(ind, offices[ind]))
+#         print('\t\tMinimal distance: {}'.format(min_dist))
+#     return min_dist, offices[ind]
+#
+#
+# offices = [[-10, 10], [20, -20], [-30, -30]]
+# cluster_centers = [[5, 5], [10,10], [0,0]]
+# # dist, office = dist_to_nearest_office(offices, cluster_centers[0], True)
+# # print('dist: {}, office: {}'.format(dist, office))
+#
+# dists = []
+# for point in cluster_centers:
+#     dist, office = dist_to_nearest_office(offices, point)
+#     dists.append([dist, office])
+# print(dists)
+
+
+coordinates_of_offices = [[25.867736, -80.324116],
+                          [51.503016, -0.075479],
+                          [52.378894, 4.885084],
+                          [39.366487, 117.036146],
+                          [-33.868457, 151.205134]]
+
+print(coordinates_of_offices[1][0])
