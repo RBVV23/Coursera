d80e78d6627f60829197c915224d1c04aea7ddb3
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-26 16:06:11 +0300

    2609 1606

--- Поиск структуры в данных/week_4/sandbox_4.py
+++ Поиск структуры в данных/week_4/sandbox_4.py
@@ -3,12 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sympy as sym
 
-def my_polinom_gen(degree=3, maxabs=10, int=True):
+def my_polinom_gen(degree=3, maxabs=10, answer=True, int=True):
     a = np.random.randint(-maxabs,maxabs,degree)
     sym.var('x')
     f = 1
     for i in range(degree):
         f *= (x-a[i])
+    if answer:
+        print(a)
     return sym.expand(f)
 sym.var('a')
 sym.var('b')
@@ -19,5 +21,10 @@ sym.var('x')
 print(sym.factor(x**3 - 6*x**2 - 67*x + 360))
 # print(np.random.randint(0,3,2))
 print(my_polinom_gen(3))
+g = sym.factor(x**3 - 6*x**2 - 67*x + 360)
+print(str(g)[:5])
+
+print(sym.expand((2*x+1)**3))
+print(sym.expand((x+2)**3))
 
 
