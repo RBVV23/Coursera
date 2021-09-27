7342461c1f926a2bd5d3e81bc4e8c7ac2bde325d
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-27 23:32:20 +0300

    2709 2332

--- Поиск структуры в данных/week_4/Project_1/main_3.py
+++ Поиск структуры в данных/week_4/Project_1/main_3.py
@@ -1,3 +1,8 @@
 import json
 from gensim import corpora, models
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+
+estimator = RandomForestClassifier(
+cross_val_score()
