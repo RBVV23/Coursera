713c5c566610fe837e1db56260bec27536e3103c
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-30 11:17:41 +0300

    3009 1117

--- Поиск структуры в данных/week_4/Project_1/main_3.py
+++ Поиск структуры в данных/week_4/Project_1/main_3.py
@@ -4,5 +4,4 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
 
-estimator = RandomForestClassifier(
-cross_val_score()
+
