7da38cd142a513968348e102434bb045d8d29d17
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-09-30 23:35:42 +0300

    3009 2335

--- Поиск структуры в данных/week_4/Project_1/main_3.py
+++ Поиск структуры в данных/week_4/Project_1/main_3.py
@@ -1,7 +1,21 @@
 import json
 from gensim import corpora, models
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score
 
 
+
+with open("recipes.json") as f:
+    recipes = json.load(f)
+print('recipes[0]:')
+print(recipes[0])
+
+texts = [recipe["ingredients"] for recipe in recipes]
+dictionary = corpora.Dictionary(texts)
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+print('texts[0] и corpus[0]:')
+print(texts[0])
+print(corpus[0])
+
+np.random.seed(76543)
+lda = models.ldamodel.LdaModel(corpus, num_topics=40, id2word=dictionary, passes=5)
