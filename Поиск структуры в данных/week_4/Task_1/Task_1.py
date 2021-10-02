f3994fa9c4fe6a8c1fc467a223e76839b1c76930
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-02 18:44:27 +0300

    0210 1844

--- Поиск структуры в данных/week_4/Task_1/Task_1.py
+++ Поиск структуры в данных/week_4/Task_1/Task_1.py
@@ -30,3 +30,68 @@ model_artm.regularizers.add(artm.SmoothSparsePhiRegularizer(name='SparsePhi2',
                                                             tau=-1e5, dictionary=dictionary,
                                                             class_ids='text',
                                                             topic_names=topic_names[:-1]))
+
+model_artm.fit_offline(batch_vectorizer=batch_vectorizer, num_collection_passes=15)
+
+for topic_name in model_artm.topic_names:
+    print(topic_name + ': '),
+    tokens = model_artm.score_tracker["TTTScore"].last_tokens
+    line = ''
+    for word in tokens[topic_name]:
+        line += word + ' '
+    print('\t', line)
+
+print()
+
+for topic_name in model_artm.topic_names:
+    print(topic_name + ': '),
+    tokens = model_artm.score_tracker["TTAScore"].last_tokens
+    line = ''
+    for word in tokens[topic_name]:
+        line += word + ' '
+    print('\t', line)
+
+sbj_topic_labels = [
+    'Физика элемантарных частиц', #0
+    'Химия полимеров', #1
+    'Политика Средневековья', #2
+    'Высшее образование', #3
+    'Нейробиология', #4
+    'Греческая мифология', #5
+    'Русская литература', #6
+    'Расовая теория', #7
+    'Римское право', #8
+    'Медицина', #9
+    'Клеточная биология', #10
+    'Астрономия', #11
+    'История России', #12
+    'Обществознание*', #13
+    'Строение Земли', #14
+    'Экономика', #15
+    'Социология', #16
+    'Искусственный интеллект', #17
+    'История Древнего мира', #18
+    'Генетика', #19
+    'Квантовая физика', #20
+    'Фмлософия', #21
+    'История Франции', #22
+    'История СССР', #23
+    'Христианство и письменность', #24
+    'Линейная алгебра', #25
+    'Финансы и феминизм', #26
+    'Теория Большого взрыва', #27
+    'Кинематограф', #28
+]
+topic_labels = sbj_topic_labels + ["Фоновая тема"]
+print('topic_labels:')
+print(topic_labels)
+
+model_artm.theta_columns_naming = "title"
+theta = model_artm.get_theta()
+phi_a = model_artm.get_phi(class_ids='author')
+print('theta.shape: ', theta.shape)
+print('phi_a.shape:', phi_a.shape)
+
+plt.figure(figsize=(20,10))
+seaborn.heatmap(theta[theta.columns[0:100]])
+plt.show()
