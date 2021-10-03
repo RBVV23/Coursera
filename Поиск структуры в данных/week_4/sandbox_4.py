101dfde93dc13aac3a670740cc5b3df236ee0bac
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-03 22:25:26 +0300

    0210 2225

--- Поиск структуры в данных/week_4/sandbox_4.py
+++ Поиск структуры в данных/week_4/sandbox_4.py
@@ -19,12 +19,13 @@ sym.var('a')
 sym.var('b')
 sym.var('x')
 
-print(sym.expand(f))
-print(sym.simplify(g))
+
 print(sym.factor(x**3 - 6*x**2 - 67*x + 360))
 print(np.random.randint(0,3,2))
 print(my_polinom_gen(3))
-g = sym.factor(x**3 - 6*x**2 - 67*x + 360)
+# g = sym.factor(x**3 - 6*x**2 - 67*x + 360)
+# print(sym.expand(f))
+# print(sym.simplify(g))
 # print(str(g)[:5])
 
 print(sym.expand((2*x+1)**3))
