453b354bd6766a2c4100b17c589c331665b1cd4e
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-08 23:18:45 +0300

    0810 2318

--- Построение выводов по данным/Week_1/sandbox_1.py
+++ Построение выводов по данным/Week_1/sandbox_1.py
@@ -1,7 +1,21 @@
 import pandas as pd
-print(pd.__version__)
+# print(pd.__version__)
 import numpy as np
-print(np.__version__)
+# print(np.__version__)
 import matplotlib.pyplot as plt
 
+def my_interval(X, alpha=0.95, norm=True):
+    sample_mean=X.mean()
+    print('sample_mean = ', sample_mean)
+    sample_std = X.std(ddof=1)
+    print('sample_std = ', sample_std)
+    low = sample_mean - 2*sample_std
+    high = sample_mean + 2 * sample_std
+    print('Доверительный интервал {}%: ({} - {})'.format(100*alpha, low, high))
+    return low, high
 
+A = np.array([3,2,3,4,3])
+print(A.mean())
+print(A.std(ddof=1))
+
+my_interval(A)
