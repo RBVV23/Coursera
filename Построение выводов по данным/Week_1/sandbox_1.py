8b40d40722dd53f9e13fb451cd2164c331756e05
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-09 19:47:07 +0300

    0910 1946

--- Построение выводов по данным/Week_1/sandbox_1.py
+++ Построение выводов по данным/Week_1/sandbox_1.py
@@ -6,18 +6,20 @@ import matplotlib.pyplot as plt
 from conda_verify.utilities import sel_pat
 
 
-def my_interval(X, alpha=0.95, norm=True):
+def my_interval(X, alpha=0.95, precision=4, norm=True):
     sample_mean=X.mean()
     print('sample_mean = ', sample_mean)
     sample_std = X.std(ddof=1)
     print('sample_std = ', sample_std)
     low = sample_mean - 2*sample_std
+    low = round(low, precision)
     high = sample_mean + 2 * sample_std
+    high = round(high, precision)
     print('Доверительный интервал {}%: ({} - {})'.format(100*alpha, low, high))
     return low, high
 
 A = np.array([3,2,3,4,3])
-# my_interval(A)
+my_interval(A)
 
 water = pd.read_csv('water.txt', sep='\t', header=0)
 
