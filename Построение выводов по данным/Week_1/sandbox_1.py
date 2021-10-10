c616f66918156499aa1980aeff949ab68deb65be
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-10 23:11:08 +0300

    1010 2311

--- Построение выводов по данным/Week_1/sandbox_1.py
+++ Построение выводов по данным/Week_1/sandbox_1.py
@@ -1,15 +1,17 @@
 import pandas as pd
-# print(pd.__version__)
+print(pd.__version__)
 import numpy as np
-# print(np.__version__)
+print(np.__version__)
 
 
 def my_interval(X, alpha=0.95, precision=4, norm=True, flag=True):
     sample_mean=X.mean()
     sample_std = X.std(ddof=1)
+
     if flag:
         print('sample_mean = ', sample_mean)
         print('sample_std = ', sample_std)
+
     low = sample_mean - 2*sample_std
     low = round(low, precision)
     high = sample_mean + 2 * sample_std
