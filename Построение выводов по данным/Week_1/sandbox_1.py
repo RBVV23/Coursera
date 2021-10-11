ccbd410b19d923d49dba064c4bb4a2490b23998c
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-11 13:51:15 +0300

    1110 1351

--- Построение выводов по данным/Week_1/sandbox_1.py
+++ Построение выводов по данным/Week_1/sandbox_1.py
@@ -32,26 +32,37 @@ print(water.head())
 
 
 
-var = water['mortality']
+my_X = water['mortality']
 # print(type(var))
 
 print('2. Постройте 95% доверительный интервал для средней годовой смертности в больших городах:')
-my_interval(var, precision=4)
+my_mean = my_X.mean()
+my_std = my_X.std(ddof=1)/sqrt(len(my_X))
+print(_tconfint_generic(my_mean, my_std, len(my_X) - 1, 0.05, 'two-sided'))
+my_interval(my_X, precision=4)
 
 print('3. Постройте 95% доверительный интервал для средней годовой смертности по всем южным городам:')
 df = water[water['location'] == 'South']
+my_X = df['mortality']
+my_mean = my_X.mean()
+my_std = my_X.std(ddof=1)/sqrt(len(my_X))
+print(_tconfint_generic(my_mean, my_std, len(my_X) - 1, 0.05, 'two-sided'))
 my_interval(df['mortality'], precision=4)
 
 print('4. Постройте 95% доверительный интервал для средней годовой смертности по всем северным городам')
 df = water[water['location'] == 'North']
+my_X = df['mortality']
+my_mean = my_X.mean()
+my_std = my_X.std(ddof=1)/sqrt(len(my_X))
+print(_tconfint_generic(my_mean, my_std, len(my_X) - 1, 0.05, 'two-sided'))
 my_interval(df['mortality'], precision=4)
 
 print('5. 95% доверительные интервалы для средней жёсткости воды в северных и южных городах:')
 
+print(_tconfint_generic(water[water.location == 'North'].hardness.mean(), water[water.location == 'North'].hardness.std(ddof=1) / np.sqrt(len(water[water.location == 'North'])), len(water[water.location == 'North']) - 1, 0.05, 'two-sided'))
 df = water[water['location'] == 'North']
 my_interval(df['hardness'], precision=4)
-print(_tconfint_generic(water[water.location == 'North'].hardness.mean(), water[water.location == 'North'].hardness.std(ddof=1) / np.sqrt(len(data[data.location == 'North'])), len(data[data.location == 'North']) - 1, 0.05, 'two-sided'))
+
+print(_tconfint_generic(water[water.location == 'South'].hardness.mean(), water[water.location == 'South'].hardness.std(ddof=1) / np.sqrt(len(water[water.location == 'South'])), len(water[water.location == 'South']) - 1, 0.05, 'two-sided'))
 df = water[water['location'] == 'South']
 my_interval(df['hardness'], precision=4)
-
-print(_tconfint_generic(water[water.location == 'South'].hardness.mean(), water[water.location == 'South'].hardness.std(ddof=1) / np.sqrt(len(data[data.location == 'South'])), len(data[data.location == 'South']) - 1, 0.05, 'two-sided'))
