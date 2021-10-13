ef27802e2f981f498b80edab15f8f78284f35cf7
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-13 23:44:10 +0300

    1310 2344

--- Построение выводов по данным/Week_1/sandbox_1.py
+++ Построение выводов по данным/Week_1/sandbox_1.py
@@ -9,6 +9,22 @@ from statsmodels.stats.proportion import proportion_confint
 from statsmodels.stats.proportion import samplesize_confint_proportion
 import matplotlib.pyplot as plt
 
+def my_proportions_confint_diff_rel(sample1, sample2, alpha = 0.05):
+    z = stats.norm.ppf(1 - alpha/2.)
+    sample = list(zip(sample1, sample2))
+    n = len(sample)
+    f = 0
+    g = 0
+    for x in sample:
+        if x[0] == 1 and x[1] == 0:
+            f += 1
+        if x[0] == 0 and x[1] == 1:
+            g += 1
+    # print('f = ',f)
+    # print('g = ',g)
+    low = float(f - g)/n - z*sqrt(float((f + g)) / n**2 - float((f - g))**2 / n**3)
+    high = float(f - g)/n + z*sqrt(float((f + g)) / n**2 - float((f - g))**2 / n**3)
+    return low, high
 
 def my_interval(X, alpha=0.95, precision=4, norm=True, flag=False):
     n = len(X)
@@ -33,77 +49,97 @@ def my_interval(X, alpha=0.95, precision=4, norm=True, flag=False):
 # my_interval(A)
 
 
-water = pd.read_csv('water.txt', sep='\t', header=0)
-# print(water.head())
+# water = pd.read_csv('water.txt', sep='\t', header=0)
+# # print(water.head())
+#
+# my_X = water['mortality']
+#
+# print('1.2. Постройте 95% доверительный интервал для средней годовой смертности в больших городах:')
+# my_mean = my_X.mean()
+# my_std = my_X.std(ddof=1)/sqrt(len(my_X))
+# print(_tconfint_generic(my_mean, my_std, len(my_X) - 1, 0.05, 'two-sided'))
+# my_interval(my_X, precision=4)
+#
+# print('1.3. Постройте 95% доверительный интервал для средней годовой смертности по всем южным городам:')
+# df = water[water['location'] == 'South']
+# my_X = df['mortality']
+# my_mean = my_X.mean()
+# my_std = my_X.std(ddof=1)/sqrt(len(my_X))
+# print(_tconfint_generic(my_mean, my_std, len(my_X) - 1, 0.05, 'two-sided'))
+# my_interval(df['mortality'], precision=4)
+#
+# print('1.4. Постройте 95% доверительный интервал для средней годовой смертности по всем северным городам')
+# df = water[water['location'] == 'North']
+# my_X = df['mortality']
+# my_mean = my_X.mean()
+# my_std = my_X.std(ddof=1)/sqrt(len(my_X))
+# print(_tconfint_generic(my_mean, my_std, len(my_X) - 1, 0.05, 'two-sided'))
+# my_interval(df['mortality'], precision=4)
+#
+# print('1.5. 95% доверительные интервалы для средней жёсткости воды в северных и южных городах:')
+#
+# print(_tconfint_generic(water[water.location == 'North'].hardness.mean(), water[water.location == 'North'].hardness.std(ddof=1) / np.sqrt(len(water[water.location == 'North'])), len(water[water.location == 'North']) - 1, 0.05, 'two-sided'))
+# df = water[water['location'] == 'North']
+# my_interval(df['hardness'], precision=4)
+#
+# print(_tconfint_generic(water[water.location == 'South'].hardness.mean(), water[water.location == 'South'].hardness.std(ddof=1) / np.sqrt(len(water[water.location == 'South'])), len(water[water.location == 'South']) - 1, 0.05, 'two-sided'))
+# df = water[water['location'] == 'South']
+# my_interval(df['hardness'], precision=4)
+#
+# print('2.2. Нормальный доверительный интервал для доли носителей варианта 13910T в популяции майя:')
+#
+# sample_mean = 1/50
+# n = 50
+# sample_std = sqrt((49*(1/50)**2 + (49/50)**2)/(n-1))
+# z = stats.norm.ppf(0.975)
+# low = sample_mean - z*sqrt(sample_mean*(1-sample_mean)/n)
+# high = sample_mean + z*sqrt(sample_mean*(1-sample_mean)/n)
+# print('[{}; {}]'.format(low, high))
+# # print(_tconfint_generic(sample_mean, sample_std, n, 0.05, 'two-sided'))
+#
+# print('2.3. Вильсоновский доверительный интервал для доли носителей варианта 13910T в популяции майя:')
+# wilson_interval = proportion_confint(count=1, nobs=50, method='wilson')
+# print('[{}; {}]'.format(wilson_interval[0], wilson_interval[1]))
+#
+# print('2.5. Объём выборки для оценки нормального интервала с точностью до 0.01:')
+# n_samples = int(np.ceil(samplesize_confint_proportion(proportion=sample_mean,
+#                                                       half_length=0.01)))
+# print(n_samples)
+#
+# array = np.arange(0, 1.01, 0.01)
+# print(array)
+# counts = list(map(lambda x: int(np.ceil(samplesize_confint_proportion(proportion=x,
+#                                                       half_length=0.01))), array))
+# print(counts)
+#
+# print('2.5. Объём выборки необходимый в худшем случае для оценки нормального интервала:')
+# print('{} человек (при p = {})'.format(np.max(counts), array[np.argmax(counts)]))
+# plt.plot(array, counts)
+# plt.grid(True)
+# plt.show()
+#
+# my_A = np.zeros(50)
+# my_A[0] = 1
+# print(my_A.std(ddof=1))
+# print(sqrt((49*(1/50)**2 + (49/50)**2)/(n-1)))
+# my_interval(my_A)
 
-my_X = water['mortality']
+print('3.1. Уточненное правило 3-х ({}) сигм'.format(stats.norm.ppf((1 + 0.997)/2.)))
+n_asp = 11037
+n_asp_inf = 104
+n_plac = 11034
+n_plac_inf = 189
+group_asp = np.array([1]*n_asp_inf + [0]*(n_asp-n_asp_inf))
+group_plac = np.array([1]*n_plac_inf + [0]*(n_asp-n_plac_inf))
+print('3.5. Вероятность инфаркта снижается при приёме аспирина на величину:')
+print(n_plac_inf/n_plac - n_asp_inf/n_asp)
 
-print('1.2. Постройте 95% доверительный интервал для средней годовой смертности в больших городах:')
-my_mean = my_X.mean()
-my_std = my_X.std(ddof=1)/sqrt(len(my_X))
-print(_tconfint_generic(my_mean, my_std, len(my_X) - 1, 0.05, 'two-sided'))
-my_interval(my_X, precision=4)
+print('3.6. Доверительный интервал для снижения вероятности инфаркта при приёме аспирина:')
+print(my_proportions_confint_diff_rel(group_plac, group_asp, alpha = 0.03))
 
-print('1.3. Постройте 95% доверительный интервал для средней годовой смертности по всем южным городам:')
-df = water[water['location'] == 'South']
-my_X = df['mortality']
-my_mean = my_X.mean()
-my_std = my_X.std(ddof=1)/sqrt(len(my_X))
-print(_tconfint_generic(my_mean, my_std, len(my_X) - 1, 0.05, 'two-sided'))
-my_interval(df['mortality'], precision=4)
-
-print('1.4. Постройте 95% доверительный интервал для средней годовой смертности по всем северным городам')
-df = water[water['location'] == 'North']
-my_X = df['mortality']
-my_mean = my_X.mean()
-my_std = my_X.std(ddof=1)/sqrt(len(my_X))
-print(_tconfint_generic(my_mean, my_std, len(my_X) - 1, 0.05, 'two-sided'))
-my_interval(df['mortality'], precision=4)
-
-print('1.5. 95% доверительные интервалы для средней жёсткости воды в северных и южных городах:')
-
-print(_tconfint_generic(water[water.location == 'North'].hardness.mean(), water[water.location == 'North'].hardness.std(ddof=1) / np.sqrt(len(water[water.location == 'North'])), len(water[water.location == 'North']) - 1, 0.05, 'two-sided'))
-df = water[water['location'] == 'North']
-my_interval(df['hardness'], precision=4)
-
-print(_tconfint_generic(water[water.location == 'South'].hardness.mean(), water[water.location == 'South'].hardness.std(ddof=1) / np.sqrt(len(water[water.location == 'South'])), len(water[water.location == 'South']) - 1, 0.05, 'two-sided'))
-df = water[water['location'] == 'South']
-my_interval(df['hardness'], precision=4)
-
-print('2.2. Нормальный доверительный интервал для доли носителей варианта 13910T в популяции майя:')
-
-sample_mean = 1/50
-n = 50
-sample_std = sqrt((49*(1/50)**2 + (49/50)**2)/(n-1))
-z = stats.norm.ppf(0.975)
-low = sample_mean - z*sqrt(sample_mean*(1-sample_mean)/n)
-high = sample_mean + z*sqrt(sample_mean*(1-sample_mean)/n)
-print('[{}; {}]'.format(low, high))
-# print(_tconfint_generic(sample_mean, sample_std, n, 0.05, 'two-sided'))
-
-print('2.3. Вильсоновский доверительный интервал для доли носителей варианта 13910T в популяции майя:')
-wilson_interval = proportion_confint(count=1, nobs=50, method='wilson')
-print('[{}; {}]'.format(wilson_interval[0], wilson_interval[1]))
-
-print('2.5. Объём выборки для оценки нормального интервала с точностью до 0.01:')
-n_samples = int(np.ceil(samplesize_confint_proportion(proportion=sample_mean,
-                                                      half_length=0.01)))
-print(n_samples)
-
-array = np.arange(0, 1.01, 0.01)
-print(array)
-counts = list(map(lambda x: int(np.ceil(samplesize_confint_proportion(proportion=x,
-                                                      half_length=0.01))), array))
-print(counts)
-
-print('2.5. Объём выборки необходимый в худшем случае для оценки нормального интервала:')
-print('{} человек (при p = {})'.format(np.max(counts), array[np.argmax(counts)]))
-plt.plot(array, counts)
-plt.grid(True)
-plt.show()
-
-my_A = np.zeros(50)
-my_A[0] = 1
-print(my_A.std(ddof=1))
-print(sqrt((49*(1/50)**2 + (49/50)**2)/(n-1)))
-my_interval(my_A)
+print('3.7. Шансы инфаркта при регулярном приёме аспирина понижаются в (раз):')
+p_asp = n_asp_inf/n_asp
+p_plac = n_plac_inf/n_plac
+odds_asp = p_asp/(1-p_asp)
+odds_plac = p_plac/(1-p_plac)
+print(odds_plac/odds_asp)
