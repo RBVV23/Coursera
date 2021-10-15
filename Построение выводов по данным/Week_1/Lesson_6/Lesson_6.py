a923ccb7d348937c4bb50a43429be0662952b3c7
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-15 11:58:04 +0300

    1510 1157

---
+++ Построение выводов по данным/Week_1/Lesson_6/Lesson_6.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+
+from scipy import stats
+import scipy
+import matplotlib.pyplot as plt
+
+fin = open('fertility.txt', 'r')
+data = list(map(lambda x: int(x.strip()),fin.readlines()))
+
+print('data[:20]: ')
+print(data[:20])
+
+# print(range(max(data)))
+# print(max(data))
+plt.bar(range((max(data))+1), np.bincount(data), color='b', label='Количество детей')
+plt.legend()
+plt.show()
+
+l = np.mean(data)
+print('Среднее значение: ', l)
+
+observed_frequences = np.bincount(data)
+print('Наблюдаемые частоты: ')
+print((observed_frequences))
+
+expected_frequences = [len(data)*stats.poisson.pmf(x,l) for x in range(min(data), max(data)+1)]
+print('Ожидаемые частоты: ')
+print((expected_frequences))
+
+plt.bar(range(len(expected_frequences)), expected_frequences, color='b', label='Распределение Пуассона')
+plt.legend()
+plt.show()
+
+print('Вероятность получения данной (или более экстремальной) статистики при условии нулевой гипотезы (величина подчиняется распределению Пуассона):')
+result = stats.chisquare(observed_frequences, expected_frequences, ddof=1)
+print(result)
+p_value = result[1]
+print('p-value = ', p_value*100, '%')
