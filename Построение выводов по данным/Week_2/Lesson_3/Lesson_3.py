abd0504971bef9aa49dfd3607fc57a17cd5e0069
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-20 22:05:16 +0300

    2010 2205

--- Построение выводов по данным/Week_2/Lesson_3/Lesson_3.py
+++ Построение выводов по данным/Week_2/Lesson_3/Lesson_3.py
@@ -8,7 +8,28 @@ from statsmodels.stats.weightstats import zconfint
 
 import matplotlib.pyplot as plt
 
-mouses_data = pd.read_csv('mirrow_mouses.txt', header=None)
+def my_permutation_t_stat_1sample(sample, mean):
+    t_stat = sum(map(lambda x: x - mean, sample))
+    return  t_stat
+
+mouses_data = pd.read_csv('mirror_mouses.txt', header=None)
 mouses_data.columns = ['proportion_of_time']
 
 print(mouses_data)
+print(mouses_data.describe())
+
+plt.hist(mouses_data.proportion_of_time)
+# plt.show()
+
+interval = zconfint(mouses_data)
+print('95% доверительный интервал для доли времени проведенной в комнате с зеркалом:')
+print('[{}; {}]'.format(interval[0], interval[1]))
+
+print('Проверка гипотезы по критерию знаков:')
+print(sign_test(mouses_data, mu0=0.5))
+
+m0 = 0.5
+print('Проверка гипотезы по критерию знаковых рангов Вилкоксона:')
+print(stats.wilcoxon(mouses_data.proportion_of_time - m0))
+
+print(my_permutation_t_stat_1sample(mouses_data.proportion0.5))
