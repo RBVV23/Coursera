7328f25812746c5bf0dd102668fe38a2d582608a
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-21 11:55:27 +0300

    2110 1155

--- Построение выводов по данным/Week_2/Lesson_3/Lesson_3.py
+++ Построение выводов по данным/Week_2/Lesson_3/Lesson_3.py
@@ -9,19 +9,34 @@ from statsmodels.stats.weightstats import zconfint
 import matplotlib.pyplot as plt
 
 def my_permutation_t_stat_1sample(sample, mean):
-    t_stat = sum(map(lambda x: x - mean, sample))
+    t_stat = sum(list(map(lambda x: x - mean, sample)))
     return  t_stat
 
 def my_permutation_zero_distr_1sample(sample, mean, max_permutations = None):
-    centered_sample = map(lambda x: x - mean, sample)
+    centered_sample = list(map(lambda x: x - mean, sample))
     if max_permutations:
-        signs_array =set([tuple(x) for x in 2*np.random.randint(2,
+        signs_array = set([tuple(x) for x in 2*np.random.randint(2,
                                                                 size=(max_permutations,
-                                                                      len(sample -1)))])
+                                                                      len(sample))) - 1])
     else:
         signs_array = itertools.product([-1,1], repeat=len(sample))
     distr = [sum(centered_sample * np.array(signs)) for signs in signs_array]
     return distr
+
+def my_permutation_test(sample, mean, max_permutations = None, alternative = 'two-sided'):
+    if alternative not in ['two-sided', 'less', 'greater']:
+        raise ValueError('Недопустимое значения параметра "alternative"\n'
+                         'допустимо: "two-sided", "less" или "greater"')
+    t_stat = my_permutation_t_stat_1sample(sample, mean)
+    zero_distr = my_permutation_zero_distr_1sample(sample, mean, max_permutations)
+
+    if alternative == 'two-sided':
+        res = sum([1. if abs(x) >= abs(t_stat) else 0. for x in zero_distr])/len(zero_distr)
+    if alternative == 'less':
+        res = sum([1. if x <= t_stat else 0. for x in zero_distr])/len(zero_distr)
+    if alternative == 'greater':
+        res = sum([1. if x >= t_stat else 0. for x in zero_distr])/len(zero_distr)
+    return res
 
 mouses_data = pd.read_csv('mirror_mouses.txt', header=None)
 mouses_data.columns = ['proportion_of_time']
@@ -45,3 +60,12 @@ print(stats.wilcoxon(mouses_data.proportion_of_time - m0))
 
 t_stat = my_permutation_t_stat_1sample(mouses_data.proportion_of_time, 0.5)
 print('t_stat = ', t_stat)
+
+plt.hist(my_permutation_zero_distr_1sample(mouses_data.proportion_of_time, 0.5), bins=15)
+# plt.show()
+
+print('Без ограничения на количество перестановок: p_value = ', my_permutation_test(
+    mouses_data.proportion_of_time, 0.5))
+
+print('С ограничением (10000) на количество перестановок: p_value = ', my_permutation_test(
+    mouses_data.proportion_of_time, 0.5, 10000))
