24f5b3b2fe4ab061008d8c8ed56db48bffc55ed0
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-25 23:34:44 +0300

    2510 2334

---
+++ Построение выводов по данным/Week_3/sandbox_3.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+from math import sqrt
+from scipy import stats
+
+from sklearn import model_selection, metrics, linear_model, ensemble
+import itertools
+import scipy
+from statsmodels.stats.weightstats import *
+
