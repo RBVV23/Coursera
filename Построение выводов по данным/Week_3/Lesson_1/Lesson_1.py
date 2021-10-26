d50fd17c8bdd010ef50dff1f99ceb50f59cd8050
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-26 22:43:50 +0300

    2610 2243

--- Построение выводов по данным/Week_3/Lesson_1/Lesson_1.py
+++ Построение выводов по данным/Week_3/Lesson_1/Lesson_1.py
@@ -6,3 +6,12 @@ print(sales.head())
 
 products = pd.read_csv('foodmart.products.tsv', sep='\t', header=0)
 print(products.head())
+
+sales = sales.merge(products[['product_id', 'product_name']], on=['product_id'], how='inner')
+print(sales.head())
+
+sparse_sales = pd.pivot_table(sales, values='sales', index=['date', 'store_id'],
+                              columns=['product_name'], fill_value=0)
+print(sparse_sales.head())
+
+sales_correlation = sparse_sales.corr()
