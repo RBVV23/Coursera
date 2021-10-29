987165887d0991e71f93e81f31480b615a891bba
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-29 18:15:29 +0300

    2910 1815

--- Построение выводов по данным/Week_3/Lesson_2/Lesson_2.py
+++ Построение выводов по данным/Week_3/Lesson_2/Lesson_2.py
@@ -13,3 +13,41 @@ print(products.head())
 
 sales = sales.merge(products[['product_id', 'product_name']], on=['product_id'], how='inner')
 print(sales.head())
+
+sparse_sales = pd.pivot_table(sales, values='sales', index=['date', 'store_id'],
+                              columns=['product_name'], fill_value=0)
+print(sparse_sales.head())
+
+corr_data = []
+
+for i, lhs_column in enumerate(sparse_sales.columns):
+    for j, rhs_column in enumerate(sparse_sales.columns):
+        if i >= j:
+            continue
+
+        corr, p = pearsonr(sparse_sales[lhs_column], sparse_sales[rhs_column])
+        corr_data.append([lhs_column, rhs_column, corr, p])
+
+sales_correlation = pd.DataFrame.from_records(corr_data)
+sales_correlation.columns = ['product_A', 'product_B', 'corr', 'p_value']
+
+print(sales_correlation.head())
+
+print('Количество отвергнутых/принятых гипотез без поправки на множественную проверку:')
+print((sales_correlation.p_value < 0.5).value_counts())
+
+reject, p_corrected, a1, a2 = multipletests(sales_correlation.p_values, alpha=0.05, method='holm')
+sales_correlation['p_corrected'] = p_corrected
+sales_correlation['reject'] = reject
+
+print(sales_correlation.head())
+
+print('Количество отвергнутых/принятых гипотез с поправки на множественную проверку методом Холма:')
+print((sales_correlation.p_value < 0.5).value_counts())
+
+print(sales_correlation[sales_correlation.reject == True].sort_values(by='corr', ascending=False))
+
+reject, p_corrected, a1, a2 = multipletests(sales_correlation.p_values, alpha=0.05, method='fdr_bh')
+sales_correlation['p_corrected'] = p_corrected
+sales_correlation['reject'] = reject
+
