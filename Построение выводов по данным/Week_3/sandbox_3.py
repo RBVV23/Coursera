87e70d789d85348f9380ec6529eae8923cafa535
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-29 23:00:39 +0300

    2910 2300

--- Построение выводов по данным/Week_3/sandbox_3.py
+++ Построение выводов по данным/Week_3/sandbox_3.py
@@ -2,7 +2,6 @@ import pandas as pd
 from math import sqrt
 import numpy as np
 from scipy import stats
-from sklearn import model_selection, metrics, linear_model, ensemble
 import itertools
 import scipy
 from statsmodels.sandbox.stats.multicomp import multipletests
