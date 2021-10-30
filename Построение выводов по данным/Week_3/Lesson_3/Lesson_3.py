67062597eb7ce08fae413f82534906985b4d5c80
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-10-30 21:45:45 +0300

    3010 2145

--- Построение выводов по данным/Week_3/Lesson_3/Lesson_3.py
+++ Построение выводов по данным/Week_3/Lesson_3/Lesson_3.py
@@ -1,2 +1,10 @@
 import patsy
 print(patsy.__version__)
+import statsmodels
+import scipy as sc
+import numpy as np
+import pandas as pd
+import statsmodels.formula.api as smf
+import statsmodels.stats.api as sms
+from statsmodels.graphics.regressionplots import plot_leverage_resid2
+import matplotlib.pyplot as plt
