6927a6be3c5b58f8be4a2836f810644dda26d1ad
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-11-01 00:09:15 +0300

    0111 0008

--- Построение выводов по данным/Week_3/Lesson_3/Lesson_3.py
+++ Построение выводов по данным/Week_3/Lesson_3/Lesson_3.py
@@ -10,5 +10,11 @@ from statsmodels.graphics.regressionplots import plot_leverage_resid2
 import matplotlib.pyplot as plt
 
 raw = pd.read_csv('beauty.csv', sep=';', index_col=False)
-print(raw.head())
+print(raw.head(), '\n')
 
+pd.plotting.scatter_matrix(raw[['wage', 'exper', 'educ', 'looks']], alpha=0.2, figsize=(15,15), diagonal='hist')
+# plt.show()
+
+cat_features = ['union', 'goodhlth', 'black', 'female', 'married', 'service']
+for cat in cat_features:
+    print(raw[cat].value_counts())
