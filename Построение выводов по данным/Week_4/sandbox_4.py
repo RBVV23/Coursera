452057cf6c89cbe4073a4304d29e7f69d3e09d67
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-11-02 21:56:41 +0300

    0211 2156

--- Построение выводов по данным/Week_4/sandbox_4.py
+++ Построение выводов по данным/Week_4/sandbox_4.py
@@ -125,25 +125,27 @@ print('my.std = ', np.sqrt(np.sum((A - A.mean())**2)/len(A)) )
 print('np.std(ddof=1) = ', A.std(ddof=1))
 print('my.std = ', np.sqrt(np.sum((A - A.mean())**2)/(len(A)-1)) )
 
-def my_gamma_function(a,b,c,z, eps=1e-17):
-    sum = 0
+prec=15
+
+def my_gamma_function(a,b,c,z, precision=16):
+    eps = 0.1**precision
+    sum = float(0)
     k = 1
     delta = eps+1
     while delta > eps:
         mult = 1
         for l in range(k):
             mult *= (a+l)*(b+l) / ((1+l)*(c+l))
-            # print(l*10)
         delta = mult * z**k
         sum += delta
         k += 1
         # print(delta)
-    return sum + 1, eps
+    return round((sum + 1),precision-1), eps
 
 n = 1000
 t = 0.5
 print(my_gamma_function(0.5, 0.5*(n+1), 3/2, -(t**2)/n))
 
 x = 0.75
-print('arcsin(x)/x = ', np.arcsin(x)/x)
+print('arcsin(x)/x = ', round(np.arcsin(x)/x, prec))
 print('F = ', my_gamma_function(1/2, 1/2, 3/2, x**2))
