b37675f831952abf78cf77b5a08cda6327d71daf
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-11-16 23:09:11 +0300

    1611 2309

--- Построение выводов по данным/Week_4/Project_2/main_2.py
+++ Построение выводов по данным/Week_4/Project_2/main_2.py
@@ -3,4 +3,8 @@ from scipy import stats
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from statsmodels.sandbox.stats.multicomp import multipletests
+
+def my_write_answer(answer, part, number):
+    name = 'answer' + str(part) + str(number) + '.txt'
+    with open(name, 'w') as file:
+        file.write(str(answer))
