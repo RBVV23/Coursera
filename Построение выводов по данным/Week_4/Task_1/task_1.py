f1901ffb12e5842f8f4f04755792bf62d465fd9c
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-11-17 22:54:33 +0300

    1711 2254

--- Построение выводов по данным/Week_4/Task_1/task_1.py
+++ Построение выводов по данным/Week_4/Task_1/task_1.py
@@ -7,7 +7,7 @@ import numpy as np
 pd.set_option('display.width', 120)
 pd.set_option('display.max_columns', 25)
 
-data = pd.read_csv('credit_card_default_analysis.csv')
+data = pd.read_csv('credit_card_default_analysis.csv', header=0, sep=',')
 
 print(data.head())
 print(data.shape)
