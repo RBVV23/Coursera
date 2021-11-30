47d549f0a6c3929b1eb98aead37ab6cb94d4d5d3
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-11-30 23:23:43 +0300

    3011 2323

--- Построение выводов по данным/Week_4/Task_1/task_1.py
+++ Построение выводов по данным/Week_4/Task_1/task_1.py
@@ -2,14 +2,37 @@ from scipy import stats
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import math as m
+
+plt.figure(figsize=(8,8))
+
+Xt = np.arange(-10, 11, 1)
+Yt = np.arange(-10, 11, 1)
+x = np.linspace(-9, 9, 1000)
+# print(x)
+# y = -1 + np.sqrt(x+2)
+# y = -4*np.exp(-x/3)
+# y = 1 - np.log(x-4)/np.log(3)
+# y = 0.5*np.sin(x + np.pi/4)
+
+k = -0.2
+y = np.exp(k*x)
+
+Xmax = max(Xt)
+Xmin = min(Xt)
+Ymax = max(Yt)
+Ymin = min(Yt)
 
 
-pd.set_option('display.width', 120)
-pd.set_option('display.max_columns', 25)
+# plt.ax.arrow(0, 0, 1, 1)
 
-data = pd.read_csv('credit_card_default_analysis.csv', header=0, sep=',')
+plt.plot(x, y, color='r')
+plt.plot([Xmin,Xmax],[0,0], color='k')
+plt.plot([0,0],[Ymin,Ymax], color='k')
+plt.xticks(Xt)
+plt.yticks(Yt)
+plt.grid()
+plt.xlim(Xmin, Xmax)
+plt.ylim(Ymin, Ymax)
 
-print(data.head())
-print(data.shape)
-
-answer1 = None
+plt.show()
