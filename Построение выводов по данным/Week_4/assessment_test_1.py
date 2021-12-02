a3330fbe165a15dae1a369e2a79acca542a5efef
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-02 23:33:55 +0300

    0212 2333

--- Построение выводов по данным/Week_4/assessment_test_1.py
+++ Построение выводов по данным/Week_4/assessment_test_1.py
@@ -6,3 +6,8 @@ data = pd.read_csv('ab_browser_test.csv')
 
 print(data.head())
 print(data.info())
+
+exp_clicks = data[data.slot == 'exp'].shape[0]
+print('Количество кликов в группе "exp": ', exp_clicks)
+control_clicks = data[data.slot == 'control'].shape[0]
+print('Количество кликов в группе "control": ', control_clicks)
