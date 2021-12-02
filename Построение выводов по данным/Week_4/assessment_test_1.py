2eaa4ab21850a881bbd515a64bc1350530d3e342
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-02 23:45:05 +0300

    0212 2344

--- Построение выводов по данным/Week_4/assessment_test_1.py
+++ Построение выводов по данным/Week_4/assessment_test_1.py
@@ -7,10 +7,10 @@ data = pd.read_csv('ab_browser_test.csv', sep=',', header=0)
 print(data.head())
 print(data.info())
 
-# exp_clicks = data[data.slot == 'exp'].shape[0]
-# print('Количество кликов в группе "exp": ', exp_clicks)
-exp_clicks = data[data.slot == 'exp'].n_clicks.value()
-# print('Количество кликов в группе "control": ', control_clicks)
-print(exp_clicks)
+exp_clicks = data[data.slot == 'exp'].n_clicks
+print('Количество кликов в группе "exp": ', np.sum(exp_clicks.values))
+
+control_clicks = data[data.slot == 'control'].n_clicks
+print('Количество кликов в группе "control": ', np.sum(control_clicks.values))
 
 # answer1 = exp_clicks - control_clicks
