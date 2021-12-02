67d07744da55cc894a4440a013420e1920b8b566
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-02 23:36:21 +0300

    0212 2336

--- Построение выводов по данным/Week_4/assessment_test_1.py
+++ Построение выводов по данным/Week_4/assessment_test_1.py
@@ -9,5 +9,7 @@ print(data.info())
 
 exp_clicks = data[data.slot == 'exp'].shape[0]
 print('Количество кликов в группе "exp": ', exp_clicks)
-control_clicks = data[data.slot == 'control'].shape[0]
+control_clicks = data[data.slot == 'control'].n_clicks
 print('Количество кликов в группе "control": ', control_clicks)
+
+# answer1 = exp_clicks - control_clicks
