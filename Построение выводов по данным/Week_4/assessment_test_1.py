c428958dcc4ca4ae6cb8364fc3071798f9f765b9
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-02 23:40:53 +0300

    0212 2340

--- Построение выводов по данным/Week_4/assessment_test_1.py
+++ Построение выводов по данным/Week_4/assessment_test_1.py
@@ -2,14 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-data = pd.read_csv('ab_browser_test.csv')
+data = pd.read_csv('ab_browser_test.csv', sep=',', header=0)
 
 print(data.head())
 print(data.info())
 
-exp_clicks = data[data.slot == 'exp'].shape[0]
-print('Количество кликов в группе "exp": ', exp_clicks)
-control_clicks = data[data.slot == 'control'].n_clicks
-print('Количество кликов в группе "control": ', control_clicks)
+# exp_clicks = data[data.slot == 'exp'].shape[0]
+# print('Количество кликов в группе "exp": ', exp_clicks)
+exp_clicks = data[data.slot == 'exp'].n_clicks.value()
+# print('Количество кликов в группе "control": ', control_clicks)
+print(exp_clicks)
 
 # answer1 = exp_clicks - control_clicks
