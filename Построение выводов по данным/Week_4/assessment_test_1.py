2e4556ef8b4969e8cc66678f6332afa48059731b
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-03 12:45:28 +0300

    0312 1245

--- Построение выводов по данным/Week_4/assessment_test_1.py
+++ Построение выводов по данным/Week_4/assessment_test_1.py
@@ -2,15 +2,33 @@ import pandas as pd
 import numpy as np
 
 
+def my_get_boostraps_samples(data, n_samples):
+    L = len(data)
+    indices = np.random.randint(0, L, (n_samples, L))
+    samples = data[indices]
+    return samples
+
+
 data = pd.read_csv('ab_browser_test.csv', sep=',', header=0)
 
 print(data.head())
 print(data.info())
 
-exp_clicks = data[data.slot == 'exp'].n_clicks
+control = data[data.slot == 'control']
+exp = data[data.slot == 'exp']
+
+exp_clicks = exp.n_clicks
 print('Количество кликов в группе "exp": ', np.sum(exp_clicks.values))
 
-control_clicks = data[data.slot == 'control'].n_clicks
+control_clicks = control.n_clicks
 print('Количество кликов в группе "control": ', np.sum(control_clicks.values))
 
-# answer1 = exp_clicks - control_clicks
+answer1 = round((np.sum(exp_clicks.values) - np.sum(control_clicks.values))/np.sum(control_clicks.values)*100, 3)
+print('answer1 = ', answer1)
+
+np.random.seed(0)
+
+bstrap_0 = my_get_boostraps_samples(control.values, 100)
+meds_0 = list(map(np.median, bstrap_0))
+bstrap_1 = my_get_boostraps_samples(test.values, 100)
+meds_1 = list(map(np.median, bstrap_1))
