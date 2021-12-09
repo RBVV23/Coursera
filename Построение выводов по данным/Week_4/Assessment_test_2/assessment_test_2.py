9825c41ca540c3a696d6b63308b240464f878b38
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-09 23:45:34 +0300

    0912 2345

--- Построение выводов по данным/Week_4/Assessment_test_2/assessment_test_2.py
+++ Построение выводов по данным/Week_4/Assessment_test_2/assessment_test_2.py
@@ -84,11 +84,11 @@ answer1 = n
 print()
 print('answer1 = ', answer1)
 
-# print('p_values')
+# print('p_values:')
 # print(p_values)
-# print('p_values_corrected')
+# print('p_values_corrected:')
 # print(p_values_corrected)
-print('p_values_fisher')
+print('p_values_fisher:')
 print(p_values_fisher)
 
 print('Количество отвергнутых нулевых гипотез (p-value < 0.05) с учетом поправки Йетса:', n_corrected)
