d78d50cb5964100a59674b5520eb52dad648d1e2
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-09 23:38:59 +0300

    0912 2338

--- Построение выводов по данным/Week_4/Assessment_test_2/assessment_test_2.py
+++ Построение выводов по данным/Week_4/Assessment_test_2/assessment_test_2.py
@@ -84,10 +84,10 @@ answer1 = n
 print()
 print('answer1 = ', answer1)
 
-print('p_values')
-print(p_values)
-print('p_values_corrected')
-print(p_values_corrected)
+# print('p_values')
+# print(p_values)
+# print('p_values_corrected')
+# print(p_values_corrected)
 print('p_values_fisher')
 print(p_values_fisher)
 
