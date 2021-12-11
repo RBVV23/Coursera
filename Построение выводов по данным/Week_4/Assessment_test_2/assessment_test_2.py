33e5d084bff87bfc908157c38e1077a3d5b788e0
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-11 23:42:12 +0300

    1112 2342

--- Построение выводов по данным/Week_4/Assessment_test_2/assessment_test_2.py
+++ Построение выводов по данным/Week_4/Assessment_test_2/assessment_test_2.py
@@ -36,7 +36,7 @@ n_corrected = 0
 p_values_corrected = []
 n_fisher = 0
 p_values_fisher = []
-f1, f2, f3 = 0,0,0
+f1, f2, f3 = 0, 0, 0
 g1, g2, g3 = 0, 0, 0
 
 for n1, st1 in enumerate(all_states):
