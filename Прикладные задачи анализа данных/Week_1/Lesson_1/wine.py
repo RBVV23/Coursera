97943371efb3e01e52dc0e725bc98561bbfa21e4
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-15 23:38:54 +0300

    1512 2338

--- Прикладные задачи анализа данных/Week_1/Lesson_1/wine.py
+++ Прикладные задачи анализа данных/Week_1/Lesson_1/wine.py
@@ -146,7 +146,8 @@ plt.legend()
 # plt.show()
 
 wine2 = wine[['sales']]
-date_list = [datetime.datetime.strptime("1994-09-01", "%Y-%m-%d") + relativedelta(year=x%12, month=x//12) for x in range(36)]
+# date_list = [datetime.datetime.strptime("1994-09-01", "%Y-%m-%d") + relativedelta(year=1994+x//12, month=9+x%12) for x in range(36)]
+date_list = [datetime.datetime.strptime("1994-09-01", "%Y-%m-%d") + relativedelta(year=1994+x//12, month=(9+x)%12) for x in range(36)]
 print(date_list)
 future = pd.DataFrame(index=date_list, columns=wine2.columns)
 wine2 = pd.concat([wine2, future])
