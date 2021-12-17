22160bef267f3e9542b1550bebcc5a095ba7687d
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-18 01:14:03 +0300

    1812 0113

--- Прикладные задачи анализа данных/Week_1/Task_1/average_wages_forecasting.py
+++ Прикладные задачи анализа данных/Week_1/Task_1/average_wages_forecasting.py
@@ -9,16 +9,19 @@ from itertools import product
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = pd.read_csv('WAG_C_M.csv', sep=';', index_col=['month'], parse_dates=['month'],
-                   dayfirst=True)
+# data = pd.read_csv('WAG_C_M.csv', sep=';', index_col=['month'], parse_dates=['month'],
+#                    dayfirst=True)
 
-# old_data = pd.read_csv('WAG_C_M.csv', sep=';', index_col=['month'], parse_dates=['month'],
-#                    dayfirst=True)
-# new_data = pd.read_csv('new_WAG_C_M.csv', sep=';', index_col=['month'], parse_dates=['month'],
-#                    dayfirst=True)
-# data = pd.concat([old_data, new_data])
+old_data = pd.read_csv('WAG_C_M.csv', sep=';', index_col=['month'], parse_dates=['month'],
+                   dayfirst=True)
+new_data = pd.read_csv('new_WAG_C_M.csv', sep=';', index_col=['month'], parse_dates=['month'],
+                   dayfirst=True)
+data = pd.concat([old_data, new_data])
+
+# data = data[0:297]
 
 print(data)
+print()
 
 plt.figure(figsize=(12,7))
 data['WAG_C_M'].plot()
@@ -64,7 +67,7 @@ print('\t d = ', d)
 plt.figure(figsize=(12,7))
 ax = plt.subplot(2,1,1)
 sm.graphics.tsa.plot_acf(data.WAG_C_M_boxcox_diff[(S+1):].values.squeeze(), lags=4*S, ax=ax)
-q, q_mark = 5, 0.12810040
+q, q_mark = 1, -0.1366
 plt.plot(q, q_mark, marker='o', markerfacecolor=(1,1,0,0), markeredgecolor='r', markeredgewidth=3, markersize=15)
 plt.text(q+0.1, 0.25, 'q = {}'.format(q))
 Q, Q_mark = 0, 1
@@ -104,31 +107,31 @@ parametrs = product(ps, qs, Ps, Qs)
 parametrs_list = list(parametrs)
 print('Количество моделей для перебора: ', len(parametrs_list), '\n')
 
-results = []
-best_aic = float('inf')
-warnings.filterwarnings('ignore')
-
-for i, param in enumerate(parametrs_list):
-    try:
-        model = sm.tsa.statespace.SARIMAX(data.WAG_C_M_boxcox, order=(param[0], d, param[1]),
-                                          seasonal_order=(param[2], D, param[3], S)).fit(disp=-1)
-    except ValueError:
-        print('Параметры невозможные для обучения: ', param)
-        continue
-    aic = model.aic
-    print('#',i)
-    if aic < best_aic:
-        best_model = model
-        best_aic = aic
-        best_param = param
-        print('\taic = {}'.format(aic))
-    results.append([param, model.aic])
-
-warnings.filterwarnings('default')
-
-result_table = pd.DataFrame(results)
-result_table.columns = ['parameters', 'aic']
-print(result_table.sort_values(by = 'aic', ascending=True).head())
+# results = []
+# best_aic = float('inf')
+# warnings.filterwarnings('ignore')
+#
+# for i, param in enumerate(parametrs_list):
+#     try:
+#         model = sm.tsa.statespace.SARIMAX(data.WAG_C_M_boxcox, order=(param[0], d, param[1]),
+#                                           seasonal_order=(param[2], D, param[3], S)).fit(disp=-1)
+#     except ValueError:
+#         print('Параметры невозможные для обучения: ', param)
+#         continue
+#     aic = model.aic
+#     print('#',i)
+#     if aic < best_aic:
+#         best_model = model
+#         best_aic = aic
+#         best_param = param
+#         print('\taic = {}'.format(aic))
+#     results.append([param, model.aic])
+#
+# warnings.filterwarnings('default')
+#
+# result_table = pd.DataFrame(results)
+# result_table.columns = ['parameters', 'aic']
+# print(result_table.sort_values(by = 'aic', ascending=True).head())
 
 # print()
 # print(best_model.summary())
--- Прикладные задачи анализа данных/Week_1/Task_1/for_new_data.py
+++ Прикладные задачи анализа данных/Week_1/Task_1/for_new_data.py
@@ -12,17 +12,43 @@ print(new_data.columns)
 new_data.drop(['WAG_M', 'WAG_M_SA'], axis=1, inplace=True)
 
 # print(new_data.head())
-print(new_data.head(-5))
+# new_data.columns = ['WAG_M', 'WAG_M_SA', 'WAG_C_M']
+print(new_data)
 first_date = new_data.month[0]
 print('first_date = ', first_date)
 # new_data.drop(['month'], axis=1, inplace=True)
-
-date_list = [datetime.datetime.strptime("2016.01.01","%Y.%m.%d") + relativedelta(year=2016+(x)//12, month=1+(x)%12) for x in range(new_data.shape[0])]
-print(date_list[:5])
-# new_data.index.name = 'month'
+#
+date_list = [datetime.datetime.strptime("1993.01.01","%Y.%m.%d") + relativedelta(year=1993+(x)//12, month=1+(x)%12) for x in range(new_data.shape[0])]
+# print(date_list[:5])
+# # new_data.index.name = 'month'
 new_data.month = list(map(lambda x: x.strftime("%Y.%m.%d"), date_list))
+print('new_data')
 print(new_data)
-
-
-new_data.to_csv('new_WAG_C_M.csv', sep=';', index=False)
-
+#
+#
+#
+# new_data.to_csv('new_WAG_C_M.csv', sep=';', index=False)
+#
+# old_data = pd.read_csv('WAG_C_M.csv', sep=';', index_col=['month'], parse_dates=['month'],
+#                    dayfirst=True)
+#
+# # old_data['month'] = old_data.index
+# old_data.insert(loc=0, column='month', value=old_data.index)
+#
+#
+# old_data.index=list(range(old_data.shape[0]))
+# print('old_data')
+# print(old_data)
+#
+#
+#
+#
+# updated_data = old_data.append(new_data)
+#
+# updated_data.index=list(range(updated_data.shape[0]))
+# print('updated_data')
+# print(updated_data)
+#
+#
+# updated_data.to_csv('update_WAG_C_M.csv', sep=';', index=False)
+#
