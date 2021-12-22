1f1700f857adeb48c6ea52de9fad255a29c8c826
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-22 23:36:16 +0300

    2112 2336

--- Прикладные задачи анализа данных/Week_1/sandbox_1.py
+++ Прикладные задачи анализа данных/Week_1/sandbox_1.py
@@ -19,7 +19,7 @@ print('answer 1.4 = ', answer14)
 
 data['n_days'] = list(map(lambda x: x.days_in_month, data.month))
 data['avg_per_day'] = data.milk / data.n_days
-# print(data.head())
+print(data.head())
 
 plt.figure()
 plt.title('average milk per day')
