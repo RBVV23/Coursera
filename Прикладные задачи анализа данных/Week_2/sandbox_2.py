ee07a63191a930afa16272a853f4e600511942e6
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-26 22:44:12 +0300

    2612 2244

--- Прикладные задачи анализа данных/Week_2/sandbox_2.py
+++ Прикладные задачи анализа данных/Week_2/sandbox_2.py
@@ -6,3 +6,11 @@ import tensorflow as tf
 print(tf.__version__)
 import matplotlib.pyplot as plt
 
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+from PIL import Image
+import numpy as np
+from imageio import imread
+from imagenet_classes import class_names
+import sys
+from sklearn.svm import SVC
