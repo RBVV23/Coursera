760bf2c06df27d63adb97eea237624a88f550f28
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-27 23:29:00 +0300

    2712 2328

--- Прикладные задачи анализа данных/Week_2/sandbox_2.py
+++ Прикладные задачи анализа данных/Week_2/sandbox_2.py
@@ -6,8 +6,8 @@ import tensorflow as tf
 print(tf.__version__)
 import matplotlib.pyplot as plt
 
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+# import tensorflow.compat.v1 as tf
+# tf.disable_v2_behavior()
 from PIL import Image
 import numpy as np
 from imageio import imread
