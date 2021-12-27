61149604e2c976eba1e5fc6bef69ff0255b30f80
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2021-12-27 23:44:38 +0300

    2712 2329

--- Прикладные задачи анализа данных/Week_2/sandbox_2.py
+++ Прикладные задачи анализа данных/Week_2/sandbox_2.py
@@ -1,9 +1,9 @@
 import pandas as pd
-print(pd.__version__)
+# print(pd.__version__)
 import numpy as np
-print(np.__version__)
+# print(np.__version__)
 import tensorflow as tf
-print(tf.__version__)
+# print(tf.__version__)
 import matplotlib.pyplot as plt
 
 # import tensorflow.compat.v1 as tf
