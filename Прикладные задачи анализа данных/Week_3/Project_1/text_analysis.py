656eec26a9082a66a62bb712314a01b9bfc1da99
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-17 13:47:25 +0300

    1701 1347

---
+++ Прикладные задачи анализа данных/Week_3/Project_1/text_analysis.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+
+data = pd.read_csv('SMSSpamCollection.txt', sep='\t', header=None)
+
+data.columns = ['content', 'sms']
+
+print(data.head())
+print(data.shape)
+print(data.describe())
+
+# labels = np.array(data.type == 'spam')
+labels = [1 if (x == 'spam') else 0 for x in data.content]
+# print(labels)
+print(type(labels))
+
+messages = list(data.sms)
+# print(messages)
+print(type(messages))
+
+X = CountVectorizer(input=messages)
+print(X)
+
