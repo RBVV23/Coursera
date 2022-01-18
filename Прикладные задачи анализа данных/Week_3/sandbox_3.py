d71896577cce625a79488b083562610c69bf3e60
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-18 17:50:20 +0300

    1801 1750

--- Прикладные задачи анализа данных/Week_3/sandbox_3.py
+++ Прикладные задачи анализа данных/Week_3/sandbox_3.py
@@ -2,3 +2,8 @@ import pandas as pd
 print(pd.__version__)
 import numpy as np
 print(np.__version__)
+
+def my_write_answer(answer, part, number):
+    name = 'answer' + str(part) + str(number) + '.txt'
+    with open(name, 'w') as file:
+        file.write(str(answer))
