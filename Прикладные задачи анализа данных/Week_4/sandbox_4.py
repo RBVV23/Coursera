29dd7e4d361bd9e02addbd82adc62bb5cc173566
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-20 23:11:42 +0300

    2001 2311

--- Прикладные задачи анализа данных/Week_4/sandbox_4.py
+++ Прикладные задачи анализа данных/Week_4/sandbox_4.py
@@ -8,3 +8,17 @@ def my_write_answer(answer, part, number):
     with open(name, 'w') as file:
         file.write(str(answer))
 
+def my_precision(d):
+    sum = 0
+    for i in d:
+        sum += i
+    return sum/len(d)
+
+def my_AP(d):
+    sum_1 = 0
+    sum_2 = 0
+    for n,i in enumerate(d):
+        sum_1 += i*i/n
+        sum_2 += i
+
+# vec_1
