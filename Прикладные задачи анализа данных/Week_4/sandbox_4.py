feca7d39d9cc2bd8d27cc4645ee611e3a5dff50a
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-20 23:44:04 +0300

    2001 2343

--- Прикладные задачи анализа данных/Week_4/sandbox_4.py
+++ Прикладные задачи анализа данных/Week_4/sandbox_4.py
@@ -15,10 +15,18 @@ def my_precision(d):
     return sum/len(d)
 
 def my_AP(d):
+    k = len(d)+1
     sum_1 = 0
     sum_2 = 0
     for n,i in enumerate(d):
-        sum_1 += i*i/n
+        sum_1 += i * i/(n+1)
         sum_2 += i
+    return sum_1/sum_2
 
-# vec_1
+vec_1 = [1, 0, 0, 1]
+vec_2 = [1, 1, 0, 0]
+
+print(my_AP(vec_1))
+print(my_AP(vec_2))
+answer11 = my_AP(vec_2) - my_AP(vec_1)
+print('answer 1.1 = ', answer11)
