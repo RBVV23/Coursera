b4bc3362ff6ebe87bd100a01b4ea727babcd5d47
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-23 22:51:13 +0300

    2301 2251

--- Прикладные задачи анализа данных/Week_4/Project_1/recommender_systems.py
+++ Прикладные задачи анализа данных/Week_4/Project_1/recommender_systems.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import numpy as np
 
+def my_write_answer(answer, part, number):
+    name = 'answer' + str(part) + str(number) + '.txt'
+    with open(name, 'w') as file:
+        file.write(str(answer))
