c5ab881a16d1407323c5b1c42f3c90d01518f64f
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-24 12:39:26 +0300

    2301 1239

--- Прикладные задачи анализа данных/Week_4/Project_1/recommender_systems.py
+++ Прикладные задачи анализа данных/Week_4/Project_1/recommender_systems.py
@@ -5,3 +5,22 @@ def my_write_answer(answer, part, number):
     name = 'answer' + str(part) + str(number) + '.txt'
     with open(name, 'w') as file:
         file.write(str(answer))
+
+pre_data = pd.read_csv('coursera_sessions_train.txt', header=None, engine='python', sep='/n')
+
+with open('coursera_sessions_train.txt') as f:
+    all_sessions = f.read().split('\n')
+
+res_sessions = all_sessions
+print(all_sessions)
+
+for i, session in enumerate(reversed(all_sessions)):
+    if session[-1] == ';':
+        res_sessions.pop(i)
+
+print(res_sessions)
+
+
+
+# print(train_data)
+# print(train_data[:10])
