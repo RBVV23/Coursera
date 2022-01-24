6efc2bbb53b670e6785d409e21ac24289aaf6fea
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-24 13:09:24 +0300

    2301 1309

--- Прикладные задачи анализа данных/Week_4/Project_1/recommender_systems.py
+++ Прикладные задачи анализа данных/Week_4/Project_1/recommender_systems.py
@@ -6,21 +6,28 @@ def my_write_answer(answer, part, number):
     with open(name, 'w') as file:
         file.write(str(answer))
 
-pre_data = pd.read_csv('coursera_sessions_train.txt', header=None, engine='python', sep='/n')
+def my_data_prepare(input_file_names):
+    for file_name in input_file_names:
+        with open(file_name) as f:
+            pre_data = f.read().split('\n')
 
-with open('coursera_sessions_train.txt') as f:
-    all_sessions = f.read().split('\n')
+        post_data = pre_data
+        print('{}:'.format(file_name))
+        print('\tЗаписей ДО обработки - {}'.format(len(pre_data)))
+        L = len(pre_data) - 1
 
-res_sessions = all_sessions
-print(all_sessions)
+        for i, session in enumerate(reversed(pre_data)):
+            if session[-1] == ';':
+                post_data.pop(L - i)
 
-for i, session in enumerate(reversed(all_sessions)):
-    if session[-1] == ';':
-        res_sessions.pop(i)
+        print('\tЗаписей ПОСЛЕ обработки - {}'.format(len(post_data)))
+        print()
 
-print(res_sessions)
+        new_name = 'res_' + file_name[9:]
+        with open(new_name, 'w') as f:
+            post_data = map(lambda x: x + '\n', post_data)
+            f.writelines(post_data)
 
+input_file_names = ['coursera_sessions_train.txt', 'coursera_sessions_test.txt']
 
-
-# print(train_data)
-# print(train_data[:10])
+my_data_prepare(input_file_names)
