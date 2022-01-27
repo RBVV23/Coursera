cf679367d29f30b2da26d33221820744dc34e0c6
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-27 15:53:00 +0300

    2701 1552

--- Прикладные задачи анализа данных/Week_3/sandbox_3.py
+++ Прикладные задачи анализа данных/Week_3/sandbox_3.py
@@ -2,11 +2,21 @@ import pandas as pd
 # print(pd.__version__)
 import numpy as np
 # print(np.__version__)
+import itertools
 
 def my_write_answer(answer, part, number):
     name = 'answer' + str(part) + str(number) + '.txt'
     with open(name, 'w') as file:
         file.write(str(answer))
+
+def my_path_choose(points, path=[]):
+    all_points = points
+    for i in range(len(points)):
+        path.append(points[i])
+        if len(points.pop(i)):
+            my_path_choose(path, points)
+        else:
+            return path
 
 
 matr = [[1,2,3],[4,5,6],[7,8,9]]
@@ -25,3 +35,27 @@ print(len(matr[0]))
 
 for i in range(len(mass)):
     print(mass[i])
+
+points = ['A','B','C','D']
+
+for p in points:
+    print(f'{p} -> ', end='')
+print()
+
+print(len(list(itertools.permutations(points, len(points)))))
+print(list(itertools.permutations(points, len(points))))
+
+print(points.pop(3))
+print(points)
+points_0 = points
+
+# my_path_choose(points)
+
+for i in range(len(points_0)):
+    print(points_0[i])
+    points = points.pop(i)
+    # for j in range(len(points)):
+    #     print('\t' + points[j])
+        # points_3 = points_2.pop(j)
+        # for k in range(len(points_3)):
+        #     print('\t\t' + points_3[k])
