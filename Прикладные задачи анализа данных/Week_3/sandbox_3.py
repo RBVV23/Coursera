0c3f2e86da9d19aa7467d5ec1b5cb0237102bfe1
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-27 12:58:31 +0300

    2701 1258

--- Прикладные задачи анализа данных/Week_3/sandbox_3.py
+++ Прикладные задачи анализа данных/Week_3/sandbox_3.py
@@ -7,3 +7,21 @@ def my_write_answer(answer, part, number):
     name = 'answer' + str(part) + str(number) + '.txt'
     with open(name, 'w') as file:
         file.write(str(answer))
+
+
+matr = [[1,2,3],[4,5,6],[7,8,9]]
+mass = [[0, 1, 2, 3],[-1, 0, 4, 5], [-2, -4, 0, 6], [-3, -5, -6, 0]]
+print(mass)
+# print(matr[1][2])
+
+
+for y in range(len(mass)):
+    for x in range(y+1, len(mass[0])):
+        print(mass[y][x], end=' ')
+
+print()
+print(len(matr))
+print(len(matr[0]))
+
+for i in range(len(mass)):
+    print(mass[i])
