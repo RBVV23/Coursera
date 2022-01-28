8f445899ffb4787ca716f6eff891f1b7d66566fe
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-28 16:46:32 +0300

    2801 1646

--- Прикладные задачи анализа данных/Week_2/sandbox_2.py
+++ Прикладные задачи анализа данных/Week_2/sandbox_2.py
@@ -2,8 +2,31 @@ import pandas as pd
 print(pd.__version__)
 import numpy as np
 print(np.__version__)
-import tensorflow as tf
-print(tf.__version__)
+# import tensorflow as tf
+# print(tf.__version__)
 
 
 
+my_set = {'A','B','C'}
+
+print(my_set)
+print(my_set)
+my_set_0 = my_set.copy()
+L = len(my_set)
+for l0 in my_set_0:
+    print(l0)
+    my_set_1=my_set.copy()
+    my_set_1.remove(l0)
+    for l1 in my_set_1:
+        print('\t', l1)
+        my_set_2 = my_set.copy()
+        my_set_2.remove(l0)
+        my_set_2.remove(l1)
+        for l2 in my_set_2:
+            print('\t\t', l2)
+    my_set.remove(l0)
+    # print('\t', my_set_2)
+
+my_seta = {'A','B','C'}
+my_setb = {'B','C'}
+print(my_seta-my_setb)
