d02447d4e4243bb4a7415fae6f290e1c6b7c5206
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-28 13:50:11 +0300

    2801 1350

--- Прикладные задачи анализа данных/Week_3/sandbox_3.py
+++ Прикладные задачи анализа данных/Week_3/sandbox_3.py
@@ -29,35 +29,62 @@ for y in range(len(mass)):
     for x in range(y+1, len(mass[0])):
         print(mass[y][x], end=' ')
 
-print()
-print(len(matr))
-print(len(matr[0]))
-
-for i in range(len(mass)):
-    print(mass[i])
-
+# print()
+# print(len(matr))
+# print(len(matr[0]))
+#
+# for i in range(len(mass)):
+#     print(mass[i])
+#
 points = ['A','B','C','D']
-
-for p in points:
-    print(f'{p} -> ', end='')
-print()
+#
+# for p in points:
+#     print(f'{p} -> ', end='')
+# print()
 
 # print(len(list(itertools.permutations(points, len(points)))))
 # print(list(itertools.permutations(points, len(points))))
 
-# print(points.pop(3))
+print(points.pop(3))
 # print(points)
 points_0 = points
 
 # my_path_choose(points)
 # print(len(points))
 
-for i in reversed(range(len(points))):
-    print(points[i])
-    points_1=points
-    points_1.pop(i)
-    for j in reversed(range(len(points_1))):
-        print('\t', points_1[j])
-        points_2 = points_1
-        points_2.pop(j)
+L = len(points) - 1 # L = 2
+for i in range(L+1):
+    # print(L-i)
+    # print(f'points = {points}')
+    # print(points[L - i], end='->')
+    points_1 = list(points)
+    # print(f'points_1 = {points_1}')
+    points_1.pop(L-i)
+    # print(f'points_1 = {points_1}')
+    # print(f'points = {points}')
+    for j in range(L):
+        # print(points_1[L-1-j], end='->')
+        # print('\t', points_1[L-1-j])
+        points_2 = list(points_1)
+        points_2.pop(L-1-j)
         for k in reversed(range(len(points_2))):
+            # print(points_2[k])
+            print(f'{points[L-i]}->{points_1[L-1-j]}->{points_2[L-2-k]}')
+            # print(f'{points[L-i]}->{points_1[L-1-j]}->{points_2[k]}')
+
+bag = ['A','B','C']
+bag_1 = ['A','B','C']
+bag_2 = ['A','B','C']
+bag_3 = ['A','B','C']
+
+for i in range(len(bag_1)):
+    print(bag_1[i])
+    bag_2 = list(bag_1)
+    bag_2.pop(i)
+    for j in range(len(bag_2)):
+        bag_3 = list(bag_2)
+        bag_3.pop(j)
+        print('\t', bag_2[j])
+        for k in range(len(bag_3)):
+            print('\t\t', bag_3[k])
+    bag.pop(i)
