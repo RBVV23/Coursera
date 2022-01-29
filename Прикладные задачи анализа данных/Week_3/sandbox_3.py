56855e134b3b652fc3c79175deb01a38d0269e9e
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-29 19:13:02 +0300

    2901 1912

--- Прикладные задачи анализа данных/Week_3/sandbox_3.py
+++ Прикладные задачи анализа данных/Week_3/sandbox_3.py
@@ -9,93 +9,126 @@ def my_write_answer(answer, part, number):
     with open(name, 'w') as file:
         file.write(str(answer))
 
-def my_path_choose(points, path=[]):
-    all_points = points
-    for i in range(len(points)):
-        path.append(points[i])
-        if len(points.pop(i)):
-            my_path_choose(path, points)
-        else:
-            return path
+points = [1, 2, 3,4]
 
-
-matr = [[1,2,3],[4,5,6],[7,8,9]]
-mass = [[0, 1, 2, 3],[-1, 0, 4, 5], [-2, -4, 0, 6], [-3, -5, -6, 0]]
-print(mass)
-# print(matr[1][2])
-
-
-for y in range(len(mass)):
-    for x in range(y+1, len(mass[0])):
-        print(mass[y][x], end=' ')
-
-# print()
-# print(len(matr))
-# print(len(matr[0]))
-#
-# for i in range(len(mass)):
-#     print(mass[i])
-#
-points = ['A','B','C','D']
-#
-# for p in points:
-#     print(f'{p} -> ', end='')
-# print()
-
-# print(len(list(itertools.permutations(points, len(points)))))
-# print(list(itertools.permutations(points, len(points))))
-
-print(points.pop(3))
-# print(points)
-points_0 = list(points)
-
-# my_path_choose(points)
-# print(len(points))
-
-# L = len(points) - 1 # L = 2
-# print('points = ', points)
-# for i in range(L+1):
-#     print('points_0 = ', points_0)
-#     # print(L-i)
-#     # print(f'points = {points}')
-#     # print(points[L - i], end='->')
-#     points_1 = list(points_0)
-#     # print(f'points_1 = {points_1}')
-#     points_1.pop(L-i)
-#     # print(f'points_1 = {points_1}')
-#     # print(f'points = {points}')
-#     print('\tpoints_1 = ', points_1)
-#     for j in range(L):
-#         # print(points_1[L-1-j], end='->')
-#         # print('\t', points_1[L-1-j])
-#         points_2 = list(points_1)
-#         points_2.pop(L-j-i)
-#         print('\t\tpoints_2 = ', points_2)
-#         for k in range(len(points_2)):
-#             # print(points_2[k])
-#             print(f'{points[L-i]}->{points_1[L-1-j]}->{points_2[L-2-k]}')
-#             # print(f'{points[L-i]}->{points_1[L-1-j]}->{points_2[k]}')
-#     # print('i =', i)
-#     points_0.pop(L-i)
-
-# bag = ['C','B','A']
-points = ['A', 'B', 'C']
-
-
-
-def func(points, track=[]):
-    if len(points)==1:
-        return track
-    track.append(points[0])
-    print(track)
-    points.pop(0)
-    return func(points, track)
-
-
-
-
-
+def func(points, tracks=[]):
+    new_tracks = tracks*(len(points)-1)
+    print(new_tracks)
+    # for i in range(len(points)-1):
+    #     new_tracks[i].append(points[:1] )
+    # func(points.pop(), tracks)
+# )
+#     i=0
+#     for track in tracks:
+#         print(track)
+#         new_track = track.append(points[0])
+#         new_tracks.append(new_track)
+    return new_tracks
 
 res = func(points)
-print(res)
+# # res[1] = [2, 1, 3, 1]
+# print(res)
 
+points = [2,3,4]
+# res2 = func(points, res)
+# print(res2)
+
+
+init = list([[]])
+# A = [[None]]
+# B = A*3
+# print(B)
+# for m in B:
+#     m.append('A')
+#     print(m)
+
+# print(res[1])
+# points = [2, 3,4]
+# res2 = func(points, res)
+# print(res2)
+
+# m =3
+# n=4
+# init =[0]
+# print()
+# A = [init * 1 for i in range(3)]
+# # A = list(A*3)
+# for i in range(len(A)):
+#     A[i].append(i+1)
+# # print(A)
+#
+# init = A
+#
+# # A = [ * 1 for i in range(3)]
+# # # A = list(A*3)
+# # for i in range(len(A)):
+# #     A[i].append(i+1)
+# # print(A)
+#
+# init = []
+# L = 3
+# M = init*L
+#
+# for i in range(L):
+#     M.append(init* 1)
+#
+# print(M)
+#
+# for i in range(len(M)):
+#     M[i].append(i+1)
+#
+# print(M)
+#
+# newM = M*2
+#
+# for i in range(len(M)):
+#     newM[i].append(i+2)
+#
+# # newM = M*2
+# print(newM)
+# # newM[0].append(10)
+# # print(newM[1])
+#
+# # init = []
+# # L = 3
+# # M = init*L
+# # for i in range(L):
+#     M.append([0]* 1)
+
+points = [1,2,3]
+nsb = 3
+nsb = len(points)
+A = []
+base = []
+for i in range(nsb):
+    A.append(base * 1)
+print(A)
+
+for i in range(len(A)):
+    A[i].append(0)
+print(A)
+print('===========================')
+for i in range(len(A)):
+    A[i].append(i+1)
+print(A)
+
+
+
+nsb = 2
+# m = 4
+# A = []
+base = A
+print('base = ', base)
+print('len(base) = ', len(base))
+print('base[1] = ', base[1])
+
+
+for i in range(len(base)*1):
+    # for j in range(len(base)-1):
+    A.append(base[i] * 1)
+
+print(A)
+
+for i in range(len(A)):
+    A[i].append((i))
+print(A)
