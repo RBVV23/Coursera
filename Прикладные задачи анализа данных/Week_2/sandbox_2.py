78b827d8ba9e04b2d7c5819a2e5c7c9bede098d1
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-01-29 03:34:41 +0300

    2801 0334

--- Прикладные задачи анализа данных/Week_2/sandbox_2.py
+++ Прикладные задачи анализа данных/Week_2/sandbox_2.py
@@ -6,29 +6,60 @@ print(np.__version__)
 # print(tf.__version__)
 
 
-my_points = ['A', 'B', 'C']
-my_points = ['A', 'B', 'C']
-my_points = ['A', 'B', 'C', 'D']
+points = ['A', 'B', 'C']
+points1 = list(points)
+track = []
 
-tracks = [['A'],['A']]
+def func(points, track=[], tracks=[]):
+    for p in points:
+        print('\t'*len(track), p)
+        track.append(p)
+        new_points = list(points)
+        new_points.remove(p)
+        # print('new_points = ', new_points, ' | ', end='\t')
+        # print('len(track) = ', len(track))
 
-# def func(points, tracks=[], track=[]):
-#     for i in range(len(points)):
-#         for j in range(len(points)):
-#             tracks[j].append(points[i])
+        if len(new_points) == 0:
+            print('track = ', track)
+            tracks.append(track)
+            print('tracks = ', tracks)
+            # track = track[:-2]
+            # print(f'func({new_points}) - из if')
+            # func(new_points, ['Aa'])
+            # func(new_points)
+            return tracks
+        # print(f'func({new_points}, {track}) - штатно')
+        func(new_points, track, tracks)
+        # track = track[:-2]
 
-
-def func(mass, new_mass=[]):
-    for i in range(len(mass)):
-        for j in range(len(mass)):
-            new_mass.append(mass[i])
-    mass.pop(0)
-    if len(mass) == 0:
-        return new_mass
-    return func(mass, new_mass)
-
-
-
-res = func(my_points)
-
-print(res)
+# res = func(points)
+# print(res)
+# print(res)
+tracks=[]
+for p1 in points1:
+    print('\t'*len(track), p1)
+    track.append(p1)
+    points2 = list(points1)
+    points2.remove(p1)
+    for p2 in points2:
+        print('\t'*len(track), p2)
+        track.append(p2)
+        points3 = list(points2)
+        points3.remove(p2)
+        for p3 in points3:
+            print('\t'*len(track), p3)
+            track.append(p3)
+            points4 = list(points3)
+            points4.remove(p3)
+            for p4 in points4:
+                print('\t'*len(track),  p4)
+                track.append(p4)
+                points5 = list(points4)
+                points5.remove(p4)
+                print(track)
+                tracks.append(track)
+            track = track[:-2]
+        track = track[:-1]
+    track = track[:-1]
+    # track = []
+print(tracks)
