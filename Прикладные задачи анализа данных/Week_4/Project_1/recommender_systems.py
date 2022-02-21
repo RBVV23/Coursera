e3015ccc8cd48664fd835f665169ece180bdd54e
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-02-21 23:38:04 +0300

    2102 2337

--- Прикладные задачи анализа данных/Week_4/Project_1/recommender_systems.py
+++ Прикладные задачи анализа данных/Week_4/Project_1/recommender_systems.py
@@ -59,16 +59,42 @@ for string in data:
     # purchase = list(map(lambda x: int(x), pre[1].split(',')))
     view = pre[0].split(',')
     purchase = pre[1].split(',')
+    for id in view:
+        view_dict[id] += 1
     views.append(view)
+    for id in purchase:
+        purchase_dict[id] += 1
     purchases.append(purchase)
     # print('\tПросмотрено: {}\n\tКуплено: {}\n'.format(view, purchase))
 
 mass = [views, purchases]
 df = pd.DataFrame(mass).T
 df.columns = ['VIEWS', 'PURCHASES']
-print(df)
+# print(df)
 
-print(data[10])
-print(df.loc[10].VIEWS)
-print(df.loc[10].PURCHASES)
+print('view_dict:')
+# print(view_dict, '\n')
+# print()
+print('purchase_dict:')
+purchase_dict.pop('')
+# print(purchase_dict)
+# print(data[10])
+# print(df.loc[10].VIEWS)
+# print(df.loc[10].PURCHASES)
 
+# for session in range(df.shape[0]//10000):
+for session in range(6,8):
+    print(df.loc[session])
+    views = df.loc[session].VIEWS
+    purchases = df.loc[session].PURCHASES
+    print('\tviews :', views)
+    print('\tpurchases: ', purchases)
+
+# print(df.loc[10])
+# print(df.shape[0])
+
+my_dict = {'23': 10, '10': 5, '8': 3, '7': 1}
+my_test = ['8', '7','8', '10', '99', '23', '99']
+# def my_sort(session):
+uni_session = np.unique(my_test)
+print(uni_session)
