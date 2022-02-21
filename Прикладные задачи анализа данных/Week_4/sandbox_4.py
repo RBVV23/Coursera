d19341ac988059b0afdc0bb7e14ac708f12e7f8f
RBVV23 <55245070+RBVV23@users.noreply.github.com>
2022-02-21 23:38:31 +0300

    2102 2338

--- Прикладные задачи анализа данных/Week_4/sandbox_4.py
+++ Прикладные задачи анализа данных/Week_4/sandbox_4.py
@@ -5,63 +5,60 @@ print(pd.__version__)
 print(np.__version__)
 
 
-def my_write_answer(answer, part, number):
-    name = 'answer' + str(part) + str(number) + '.txt'
-    with open(name, 'w') as file:
-        file.write(str(answer))
-def my_precision(d, k):
-    sum = 0
-    for i in d[:k]:
-        sum += i
-    return sum/k
-def my_AP(d):
-    k = len(d)+1
-    sum_1 = 0
-    sum_2 = 0
-    for n,i in enumerate(d):
-        sum_1 += i * my_precision(d,n+1)
-        sum_2 += i
-    return sum_1/sum_2
+# def my_write_answer(answer, part, number):
+#     name = 'answer' + str(part) + str(number) + '.txt'
+#     with open(name, 'w') as file:
+#         file.write(str(answer))
+# def my_precision(d, k):
+#     sum = 0
+#     for i in d[:k]:
+#         sum += i
+#     return sum/k
+# def my_AP(d):
+#     k = len(d)+1
+#     sum_1 = 0
+#     sum_2 = 0
+#     for n,i in enumerate(d):
+#         sum_1 += i * my_precision(d,n+1)
+#         sum_2 += i
+#     return sum_1/sum_2
+#
+#
+# vec_1 = [1, 0, 0, 1]
+# vec_2 = [1, 1, 0, 0]
+#
+# print(my_AP(vec_1))
+# print(my_AP(vec_2))
+# answer11 = my_AP(vec_2) - my_AP(vec_1)
+# print('answer 1.1 = ', answer11)
+#
+#
+# mass = ['A','B','C','D','E', 'F', 'G', 'H']
+# new_mass = mass
+# l = len(mass) - 1
+# for i, egg in enumerate(reversed(mass)):
+#     print('{}: mass[{}] = {}'.format(i, l-i, egg))
+#     if (egg == 'A') or (egg == 'C') or (egg == 'H'):
+#         new_mass.pop(l-i)
+#
+# print(new_mass)
+#
+#
+# mama = [0,1,2,3,4,5]
+# # mama.pop((3,5))
+# print(mama)
 
 
-vec_1 = [1, 0, 0, 1]
-vec_2 = [1, 1, 0, 0]
-
-print(my_AP(vec_1))
-print(my_AP(vec_2))
-answer11 = my_AP(vec_2) - my_AP(vec_1)
-print('answer 1.1 = ', answer11)
-
-
-mass = ['A','B','C','D','E', 'F', 'G', 'H']
-new_mass = mass
-l = len(mass) - 1
-for i, egg in enumerate(reversed(mass)):
-    print('{}: mass[{}] = {}'.format(i, l-i, egg))
-    if (egg == 'A') or (egg == 'C') or (egg == 'H'):
-        new_mass.pop(l-i)
-
-print(new_mass)
-
-
-mama = [0,1,2,3,4,5]
-# mama.pop((3,5))
-print(mama)
-
-
-
-
-# [[0, 1, 2, 3], [0, 2, 3, 1], [0, 3, 1, 2], [0, 1, 3, 2], [0, 2, 1, 3], [0, 3, 2, 1]]
-for i in range(24):
-    # n1 = 1 + (0+i + (i//3) )%3
-    n1 = 1 + (0+i + 0* (i //4) )%4
-    n2 = 1 + (1+i + 1* (i //4) )%4
-    n3 = 1 + (2+i + 2* (i //4) )%4
-    n4 = 1 + (3+i + 3* (i //4) )%4
-    print(f'{n1}{n2}{n3}{n4}')
-
-for i in range(6):
-    n1 = 1 + (0+i + 0*(i//3) )%3
-    n2 = 1 + (1+i + 1*(i//3) )%3
-    n3 = 1 + (2+i + 2*(i//3) )%3
-    print(f'{n1}{n2}{n3}')
+my_dict = {'23': 10, '10': 5, '8': 3, '7': 1}
+my_test = ['8', '7','8', '10', '99', '23', '99']
+# def my_sort(session):
+uni_session = np.unique(my_test)
+array = np.empty((len(uni_session),2))
+print(uni_session)
+array[:,0] = uni_session
+for i,id in enumerate(array[:,0]):
+    # array[i,1] = my_dict[id]
+    print(id)
+    ...
+print(array)
+# for id in
